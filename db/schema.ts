bf39ed4dce01c881525c9d7c42e59386588f9d9f
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-02-21 11:32:35 +1300

    Setup drizzle

---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, bigserial, text } from 'drizzle-orm/pg-core'
+import { sql } from 'drizzle-orm'
+
+export const notes = pgTable('notes', {
+  id: bigserial({ mode: 'bigint' }).primaryKey().notNull(),
+  title: text(),
+})
