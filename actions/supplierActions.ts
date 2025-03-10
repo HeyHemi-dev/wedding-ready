b162f1b379f0c1366ebf1f2d701045b751cf7e52
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-10 16:49:57 +1300

    Add location to supplier

--- actions/supplierActions.ts
+++ actions/supplierActions.ts
@@ -1,5 +1,10 @@
 import { db } from '@/db/db'
-import { suppliers as suppliersTable, supplierUsers as supplierUsersTable } from '@/db/schema'
+import {
+  suppliers as suppliersTable,
+  supplierUsers as supplierUsersTable,
+  supplierLocations as supplierLocationsTable,
+  locations as locationsTable,
+} from '@/db/schema'
 import { InsertSupplier, InsertSupplierUser, Supplier, SupplierRole, SupplierWithUsers } from '@/models/Suppliers'
 import { User, UserWithDetail } from '@/models/Users'
 import { eq } from 'drizzle-orm'
@@ -21,20 +26,44 @@ class SupplierActions {
       .select()
       .from(suppliersTable)
       .where(eq(suppliersTable.handle, handle))
-      .innerJoin(supplierUsersTable, eq(suppliersTable.id, supplierUsersTable.supplierId))
+      .leftJoin(supplierUsersTable, eq(suppliersTable.id, supplierUsersTable.supplierId))
+      .leftJoin(supplierLocationsTable, eq(suppliersTable.id, supplierLocationsTable.supplierId))
+      .leftJoin(locationsTable, eq(supplierLocationsTable.locationId, locationsTable.id))
 
     if (result.length === 0) {
       return null
     }
 
     const supplier = result[0].suppliers
-    const supplierUsers = result.map((r) => r.supplier_users)
+    const supplierUsers = result.map((r) => r.supplier_users).filter((user): user is NonNullable<typeof user> => user !== null)
+    const supplierLocations = result.map((r) => r.locations).filter((location): location is NonNullable<typeof location> => location !== null)
 
     return {
       ...supplier,
       users: supplierUsers,
+      locations: supplierLocations,
     }
   }
+  // static async getByHandle(handle: string): Promise<SupplierWithUsers | null> {
+  //   const result = await db
+  //     .select()
+  //     .from(suppliersTable)
+  //     .where(eq(suppliersTable.handle, handle))
+  //     .leftJoin(supplierUsersTable, eq(suppliersTable.id, supplierUsersTable.supplierId))
+
+  //   if (result.length === 0) {
+  //     return null
+  //   }
+
+  //   const supplier = result[0].suppliers
+  //   const supplierUsers = result.map((r) => r.supplier_users).filter((user): user is NonNullable<typeof user> => user !== null)
+
+  //   return {
+  //     ...supplier,
+  //     users: supplierUsers,
+  //     locations: [],
+  //   }
+  // }
 
   static async create(admin: User | UserWithDetail, insertSupplierData: InsertSupplier): Promise<SupplierWithUsers> {
     const suppliers = await db.insert(suppliersTable).values(insertSupplierData).returning()
@@ -52,6 +81,7 @@ class SupplierActions {
     return {
       ...supplier,
       users: supplierUsers,
+      locations: [],
     }
   }
 }
--- models/Suppliers.ts
+++ models/Suppliers.ts
@@ -1,5 +1,5 @@
 import { InferSelectModel, InferInsertModel } from 'drizzle-orm'
-import { suppliers, supplierLocations, supplierUsers } from '@/db/schema'
+import { suppliers, supplierLocations, supplierUsers, locations } from '@/db/schema'
 
 export type Supplier = InferSelectModel<typeof suppliers>
 export type InsertSupplier = InferInsertModel<typeof suppliers>
@@ -7,11 +7,15 @@ export type InsertSupplier = InferInsertModel<typeof suppliers>
 export type SupplierLocation = InferSelectModel<typeof supplierLocations>
 export type InsertSupplierLocation = InferInsertModel<typeof supplierLocations>
 
+export type Location = InferSelectModel<typeof locations>
+export type InsertLocation = InferInsertModel<typeof locations>
+
 export type SupplierUser = InferSelectModel<typeof supplierUsers>
 export type InsertSupplierUser = InferInsertModel<typeof supplierUsers>
 
 export interface SupplierWithUsers extends Supplier {
   users: SupplierUser[]
+  locations: Location[]
 }
 
 // This enum is used by the schema to define the pgEnum
