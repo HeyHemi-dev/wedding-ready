ba7581cc8b58cb9b4fe1cec957706d42d6b47988
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-11 11:11:14 +1300

    Add auth check for creating a tile

--- app/suppliers/[handle]/new/page.tsx
+++ app/suppliers/[handle]/new/page.tsx
@@ -1,6 +1,13 @@
 import Section from '@/components/ui/section'
+import { getCurrentUser } from '@/actions/get-current-user'
+import { redirect } from 'next/navigation'
 
-export default function NewSupplierTilePage() {
+export default async function NewSupplierTilePage() {
+  const user = await getCurrentUser()
+  if (!user) {
+    redirect('/sign-in')
+  }
+
   return (
     <Section>
       <h1>New Supplier Tile</h1>
