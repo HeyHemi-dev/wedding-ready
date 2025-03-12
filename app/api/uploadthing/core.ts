598d0b9c3f0bb4f241c84405a3bbe9890270b146
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-13 09:01:40 +1300

    Create a tile when upload completes

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,14 +1,15 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { UploadThingError } from 'uploadthing/server'
 import { getCurrentUser } from '@/actions/get-current-user'
-
+import TileActions from '@/models/tile-actions'
+import { InsertTileRaw } from '@/models/types'
 const f = createUploadthing()
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const uploadthingRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   tileUploader: f({
-    image: {
+    'image/jpeg': {
       /**
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
@@ -17,28 +18,29 @@ export const uploadthingRouter = {
       maxFileCount: 10,
     },
   })
-    // Set permissions and file types for this FileRoute
+    // Middleware runs on the server before upload
+    // Whatever is returned is accessible in onUploadComplete as `metadata`
     .middleware(async ({ req }) => {
-      const body = await req.json()
-      const { supplierId } = body
+      // eslint-disable-line @typescript-eslint/no-unused-vars
 
-      // Authenticate before upload
       const user = await getCurrentUser()
-
-      // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError('Unauthorized')
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id }
     })
+    // OnUploadComplete runs on the server after upload
+    // Whatever is returned is sent to the clientside `onClientUploadComplete` callback
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log('Upload complete for userId:', metadata.userId)
+      console.log('Uploaded by:', metadata.userId)
+      console.log('File url:', file.ufsUrl)
 
-      console.log('file url', file.url)
+      const tile = await TileActions.createRaw({
+        createdByUserId: metadata.userId,
+        imagePath: file.ufsUrl,
+        title: file.name,
+      })
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId }
+      return { tileId: tile.id, tileImagePath: tile.imagePath, tileTitle: tile.title }
     }),
 } satisfies FileRouter
 
