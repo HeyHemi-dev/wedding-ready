b5fe64541f2bc5136dd52d55949ef16788e76e4d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-15 19:22:09 +1300

    Update the tile with imagePath and improve error handling

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,44 +1,55 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { UploadThingError } from 'uploadthing/server'
 import { getCurrentUser } from '@/actions/get-current-user'
-import { tiles } from '@/db/schema'
 import { TileModel } from '@/models/tile'
-
+import { tileUploaderInputSchema } from '@/models/validations'
+import { error } from 'console'
 const f = createUploadthing()
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const uploadthingRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   tileUploader: f({
     'image/jpeg': {
       maxFileSize: '1MB',
-      maxFileCount: 10,
+      maxFileCount: 1,
     },
   })
+    .input(tileUploaderInputSchema)
     // Middleware runs on the server before upload
     // Whatever is returned is accessible in onUploadComplete as `metadata`
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .middleware(async ({ req }) => {
-      const user = await getCurrentUser()
-      if (!user) throw new UploadThingError('Unauthorized')
-      return {
-        userId: user.id,
+    .middleware(async ({ req, input }) => {
+      try {
+        const user = await getCurrentUser()
+        if (!user || user.id !== input.createdByUserId) throw new Error('Unauthorized')
+
+        const validInput = tileUploaderInputSchema.safeParse(input)
+        if (!validInput.success) throw new Error('Invalid input')
+
+        const tileRaw = await TileModel.getRawById(validInput.data.tileId)
+        if (!tileRaw) throw new Error('Tile not found')
+
+        return {
+          tileRaw: tileRaw,
+        }
+      } catch (error) {
+        console.error('Error in upload middleware', error)
+        throw new UploadThingError('Error while uploading tile')
       }
     })
+
     // OnUploadComplete runs on the server after upload
     // Whatever is returned is sent to the clientside `onClientUploadComplete` callback
     .onUploadComplete(async ({ metadata, file }) => {
-      console.log('Uploaded by:', metadata.userId)
-      console.log('File url:', file.ufsUrl)
-
-      // TODO: figure out a way to get the tile id through metadata
-      // const tile = await TileModel.getById(t.tileId)
-      // if (!tile) throw new Error('Tile not found')
-
-      // await TileModel.update({
-      //   ...tile,
-      //   imagePath: file.ufsUrl,
-      // })
+      try {
+        const tile = await TileModel.update({
+          ...metadata.tileRaw,
+          imagePath: file.ufsUrl,
+        })
+      } catch (error) {
+        console.error('Error updating tile', error)
+        throw new UploadThingError('Error updating tile')
+      }
 
       return
     }),
