e375ffbe0d1fb829c39e7a502bee762c5f4370eb
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-18 14:34:36 +1300

    make all set fields optional, so we don't need to update any more fields than needed

--- models/types.ts
+++ models/types.ts
@@ -15,43 +15,43 @@ export type UserDetailRaw = InferSelectModel<typeof schema.user_details>
  * @requires id - must match the id of the Supabase Auth user
  */
 export type InsertUserDetailRaw = InferInsertModel<typeof schema.user_details>
-export type SetUserDetailRaw = Omit<InsertUserDetailRaw, 'id' | 'createdAt'>
+export type SetUserDetailRaw = Partial<Omit<InsertUserDetailRaw, 'id' | 'createdAt'>>
 
 export type SupplierRaw = InferSelectModel<typeof schema.suppliers>
 export type InsertSupplierRaw = InferInsertModel<typeof schema.suppliers>
-export type SetSupplierRaw = Omit<InsertSupplierRaw, 'id' | 'createdAt' | 'createdByUserId'>
+export type SetSupplierRaw = Partial<Omit<InsertSupplierRaw, 'id' | 'createdAt' | 'createdByUserId'>>
 
 export type TileRaw = InferSelectModel<typeof schema.tiles>
 export type InsertTileRaw = InferInsertModel<typeof schema.tiles>
-export type SetTileRaw = Omit<InsertTileRaw, 'id' | 'createdAt' | 'createdByUserId' | 'isPrivate'>
+export type SetTileRaw = Partial<Omit<InsertTileRaw, 'id' | 'createdAt' | 'createdByUserId' | 'isPrivate'>>
 
 export type StackRaw = InferSelectModel<typeof schema.stacks>
 export type InsertStackRaw = InferInsertModel<typeof schema.stacks>
-export type SetStackRaw = Omit<InsertStackRaw, 'id' | 'createdAt' | 'ownedByUserId'>
+export type SetStackRaw = Partial<Omit<InsertStackRaw, 'id' | 'createdAt' | 'ownedByUserId'>>
 
 export type SupplierUserRaw = InferSelectModel<typeof schema.supplierUsers>
 export type InsertSupplierUserRaw = InferInsertModel<typeof schema.supplierUsers>
-export type SetSupplierUserRaw = Omit<InsertSupplierUserRaw, 'supplierId' | 'userId'>
+export type SetSupplierUserRaw = Partial<Omit<InsertSupplierUserRaw, 'supplierId' | 'userId'>>
 
 export type SupplierServiceRaw = InferSelectModel<typeof schema.supplierServices>
 export type InsertSupplierServiceRaw = InferInsertModel<typeof schema.supplierServices>
-export type SetSupplierServiceRaw = Omit<InsertSupplierServiceRaw, 'supplierId' | 'service'>
+export type SetSupplierServiceRaw = Partial<Omit<InsertSupplierServiceRaw, 'supplierId' | 'service'>>
 
 export type SupplierLocationRaw = InferSelectModel<typeof schema.supplierLocations>
 export type InsertSupplierLocationRaw = InferInsertModel<typeof schema.supplierLocations>
-export type SetSupplierLocationRaw = Omit<InsertSupplierLocationRaw, 'supplierId' | 'location'>
+export type SetSupplierLocationRaw = Partial<Omit<InsertSupplierLocationRaw, 'supplierId' | 'location'>>
 
 export type SavedTileRaw = InferSelectModel<typeof schema.savedTiles>
 export type InsertSavedTileRaw = InferInsertModel<typeof schema.savedTiles>
-export type SetSavedTileRaw = Omit<InsertSavedTileRaw, 'userId' | 'tileId'>
+export type SetSavedTileRaw = Partial<Omit<InsertSavedTileRaw, 'userId' | 'tileId'>>
 
 export type TileSupplierRaw = InferSelectModel<typeof schema.tileSuppliers>
 export type InsertTileSupplierRaw = InferInsertModel<typeof schema.tileSuppliers>
-export type SetTileSupplierRaw = Omit<InsertTileSupplierRaw, 'tileId' | 'supplierId'>
+export type SetTileSupplierRaw = Partial<Omit<InsertTileSupplierRaw, 'tileId' | 'supplierId'>>
 
 export type StackTileRaw = InferSelectModel<typeof schema.stackTiles>
 export type InsertStackTileRaw = InferInsertModel<typeof schema.stackTiles>
-export type SetStackTileRaw = Omit<InsertStackTileRaw, 'stackId' | 'tileId'>
+export type SetStackTileRaw = Partial<Omit<InsertStackTileRaw, 'stackId' | 'tileId'>>
 
 /**
  * UserWithDetail extends Supabase Auth user with user_details fields.
