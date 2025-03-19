dcb329519e586ad122fcc5924741d050e1be799b
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-19 13:47:25 +1300

    Update image handling with Next Image

--- components/tiles/tile-list.tsx
+++ components/tiles/tile-list.tsx
@@ -1,11 +1,12 @@
 import { Tile } from '@/models/types'
 import { Skeleton } from '@/components/ui/skeleton'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function TileList({ tiles }: { tiles: Tile[] }) {
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-md">
         {tiles.map((tile) => (
           <TileListItem key={tile.id} tile={tile} />
         ))}
@@ -16,9 +17,9 @@ export function TileList({ tiles }: { tiles: Tile[] }) {
 
 export function TileListItem({ tile }: { tile: Tile }) {
   return (
-    <div className="grid grid-rows-[auto_1fr] gap-2">
-      <Link href={`/t/${tile.id}`} className="aspect-[2/3] bg-muted rounded-lg overflow-hidden">
-        <img src={tile.imagePath} alt={tile.title} className="h-full w-full object-contain" />
+    <div className="gap-xs grid grid-rows-[auto_1fr]">
+      <Link href={`/t/${tile.id}`} className="aspect-[2/3] bg-muted rounded-lg overflow-hidden relative">
+        <Image src={tile.imagePath} alt={tile.title} fill sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw" className="object-contain" />
       </Link>
       <div className="flex flex-col gap-1">
         <p className="text-sm font-semibold">{tile.title}</p>
@@ -30,9 +31,9 @@ export function TileListItem({ tile }: { tile: Tile }) {
 
 export function TileListSkeleton() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-md">
       {Array.from({ length: 5 }).map((_, index) => (
-        <div key={index} className="grid grid-rows-[auto_1fr] gap-2">
+        <div key={index} className="grid grid-rows-[auto_1fr] gap-xs">
           <Skeleton className="aspect-[2/3] rounded-lg" />
           <div className="flex flex-col gap-1">
             <Skeleton className="h-2 w-2/3" />
