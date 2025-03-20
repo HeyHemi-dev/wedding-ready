19da0cb256a681854ed3d0a08c9b61ef103fdbe7
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-20 20:20:38 +1300

    Add an account nav

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,5 +1,30 @@
+import { Button } from '@/components/ui/button'
 import Section from '@/components/ui/section'
+import Link from 'next/link'
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  return <Section>{children}</Section>
+  return (
+    <Section>
+      <div className="grid grid-cols-1 md:grid-cols-[minmax(200px,1fr)_3fr] gap-md">
+        <nav className="flex flex-col gap-2">
+          <Link href="/account">
+            <Button variant={'ghost'}>Public Profile</Button>
+          </Link>
+          <Link href="/account/reset-password">
+            <Button variant={'ghost'}>Account Settings</Button>
+          </Link>
+          <Link href="/account/link-suppliers">
+            <Button variant={'ghost'}>Linked Suppliers</Button>
+          </Link>
+          <Link href="/account">
+            <Button variant={'ghost'}>Notifications</Button>
+          </Link>
+          <Link href="/account">
+            <Button variant={'ghost'}>Privacy</Button>
+          </Link>
+        </nav>
+        {children}
+      </div>
+    </Section>
+  )
 }
