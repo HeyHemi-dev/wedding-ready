717712a3df5dca37456a265e742a2f089e0094b0
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-03-20 19:41:37 +1300

    Link to the account settings from the profile page

--- app/u/[handle]/page.tsx
+++ app/u/[handle]/page.tsx
@@ -1,11 +1,27 @@
+import { Button } from '@/components/ui/button'
 import Section from '@/components/ui/section'
+import { UserDetailModel } from '@/models/user'
+import { getCurrentUser } from '@/actions/get-current-user'
+import Link from 'next/link'
 
 export default async function UserPage({ params }: { params: Promise<{ handle: string }> }) {
   const { handle } = await params
+  const userDetail = await UserDetailModel.getByHandle(handle)
+
+  if (!userDetail) {
+    return <div>User not found</div>
+  }
+  const user = await getCurrentUser()
+  const isCurrentUser = user?.id === userDetail.id
 
   return (
     <Section>
-      <h1>UserPage for {handle}</h1>
+      <h1>UserPage for {userDetail.handle}</h1>
+      {isCurrentUser && (
+        <Link href={`/account`}>
+          <Button>Edit Account Settings</Button>
+        </Link>
+      )}
     </Section>
   )
 }
