b515297b8239ec10079dfac582b9ac7db1160d32
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-01 17:20:53 +1300

    Remove unused import for lint

--- app/suppliers/page.tsx
+++ app/suppliers/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import Section from '@/components/ui/section'
 import { SupplierModel } from '@/models/supplier'
