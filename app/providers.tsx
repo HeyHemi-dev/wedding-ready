8813fb4d1383f6c49566d4318726ef51216978d5
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-03 11:00:09 +1300

    Add useQuery tools

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react'
 import { ThemeProvider } from 'next-themes'
 import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function makeQueryClient() {
   return new QueryClient({
@@ -44,6 +45,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
