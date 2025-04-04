ce97f9f7ca7ff53799686ea8497211e37255cd22
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-04 15:06:14 +1300

    Create a query to fetch tile save data when it is invalidated

--- hooks/use-tile.ts
+++ hooks/use-tile.ts
@@ -1,44 +1,64 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query'
 import { SaveTileRequestBody, SaveTileResponseBody } from '@/app/api/users/[id]/tiles/[tileId]/route'
 import { tryCatchFetch } from '@/utils/try-catch'
 import { toast } from 'sonner'
+import { tileKeys } from './queryKeys'
 
-export function useTile(tileId: string) {
+export function useTileSaveState(tileId: string, userId: string) {
   const queryClient = useQueryClient()
 
-  const saveUnsave = useMutation({
-    mutationFn: ({ userId, isSaved }: { userId: string; isSaved: boolean }) => fetchSaveTile(userId, tileId, isSaved),
+  const Query = useQuery({
+    queryKey: tileKeys.saveState(tileId, userId),
+    queryFn: () => fetchSaveTile(userId, tileId),
+    initialData: queryClient.getQueryData(tileKeys.saveState(tileId, userId)),
+    staleTime: Infinity,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  })
+
+  const Mutate = useMutation({
+    mutationFn: ({ userId, isSaved }: { userId: string; isSaved: boolean }) => postSaveTile(userId, tileId, isSaved),
     // handle race conditions while optimistically updating the tile's saved state, and return the previous value in case we need to roll back
     onMutate: async ({ userId, isSaved }) => {
       // Cancel any in flight
-      await queryClient.cancelQueries({ queryKey: ['saveTile', tileId, userId] })
+      await queryClient.cancelQueries({ queryKey: tileKeys.saveState(tileId, userId) })
 
-      const previousSaveState = queryClient.getQueryData(['saveTile', tileId, userId])
+      const previousSaveState = queryClient.getQueryData(tileKeys.saveState(tileId, userId))
 
       // Optimistic update
-      queryClient.setQueryData(['saveTile', tileId, userId], (old: any) => ({
-        ...old,
-        isSaved,
-      }))
+      queryClient.setQueryData(tileKeys.saveState(tileId, userId), isSaved)
 
       return { previousSaveState }
     },
     // If the mutation fails, roll back
     onError: (err, variables, context) => {
       if (context?.previousSaveState) {
-        queryClient.setQueryData(['saveTile', tileId, variables.userId], context.previousSaveState)
+        queryClient.setQueryData(tileKeys.saveState(tileId, variables.userId), context.previousSaveState)
       }
     },
     // Always refetch after error or success to ensure we have the latest data
     onSettled: (data, error, variables) => {
-      queryClient.invalidateQueries({ queryKey: ['saveTile', tileId, variables.userId] })
+      queryClient.invalidateQueries({ queryKey: tileKeys.saveState(tileId, variables.userId) })
     },
   })
 
-  return { saveUnsave }
+  return { ...Query, ...Mutate }
 }
 
-async function fetchSaveTile(userId: string, tileId: string, isSaved: boolean): Promise<SaveTileResponseBody> {
+async function fetchSaveTile(userId: string, tileId: string): Promise<SaveTileResponseBody> {
+  // const { data, error } = await tryCatchFetch<SaveTileResponseBody>(`/api/users/${userId}/tiles/${tileId}`)
+
+  // if (error) {
+  //   toast('Failed to get tile')
+  //   throw error
+  // }
+
+  // return data
+  return { isSaved: false, userId, tileId }
+}
+
+async function postSaveTile(userId: string, tileId: string, isSaved: boolean): Promise<SaveTileResponseBody> {
   const saveTileRequestBody: SaveTileRequestBody = {
     isSaved,
   }
