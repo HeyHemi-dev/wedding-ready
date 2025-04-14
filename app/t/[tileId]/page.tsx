979e8509d686da91f09975537bd1a75c3c54e6c3
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-15 09:34:09 +1200

    Update styling of tile page with link to suppliers

--- app/t/[tileId]/page.tsx
+++ app/t/[tileId]/page.tsx
@@ -8,6 +8,11 @@ import { Section } from '@/components/ui/section'
 import { tileKeys } from '@/hooks/queryKeys'
 import { TileModel } from '@/models/tile'
 import { getAuthenticatedUserId } from '@/utils/auth'
+import { valueToPretty } from '@/utils/enum-helpers'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Link as LinkIcon } from 'lucide-react'
+import { Area } from '@/components/ui/area'
 
 export default async function TilePage({ params }: { params: Promise<{ tileId: string }> }) {
   const { tileId } = await params
@@ -24,26 +29,36 @@ export default async function TilePage({ params }: { params: Promise<{ tileId: s
   }
 
   return (
-    <Section>
-      <Card>
-        <div className="grid grid-cols-2">
-          <div className="relative aspect-[2/3] overflow-hidden rounded-lg bg-muted">
-            <Image src={tile.imagePath ?? ''} alt={tile.title} fill sizes="(max-width: 768px) 100vw, 50vw" className="object-contain" />
-            {authUserId && <SaveTileButton tileId={tile.id} authUserId={authUserId} className="absolute right-0 top-0 p-2" />}
-          </div>
-          <div className="grid grid-rows-[auto_1fr_auto]">
-            <CardHeader>
-              <CardTitle>{tile.title}</CardTitle>
-              <CardDescription>{tile.location}</CardDescription>
-              <CardDescription>{tile.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <h3>Suppliers</h3>
-            </CardContent>
-            <CardFooter className="flex flex-row-reverse"></CardFooter>
-          </div>
+    <Section className="min-h-svh-minus-header">
+      <Area className="grid grid-cols-1 gap-md md:grid-cols-2">
+        <div className="relative bg-muted">
+          <Image src={tile.imagePath} alt={tile.title} fill sizes="(max-width: 768px) 100vw, 50vw" className="object-contain" />
+
+          {authUserId && <SaveTileButton tileId={tile.id} authUserId={authUserId} className="absolute right-0 top-0 p-2" />}
         </div>
-      </Card>
+        <div className="grid grid-rows-[auto_1fr_auto] gap-md">
+          <CardHeader>
+            <h1 className="font-serif text-4xl">{tile.title}</h1>
+            {tile.location && <CardDescription>{valueToPretty(tile.location as string)}</CardDescription>}
+            {tile.description && <CardDescription>{tile.description}</CardDescription>}
+          </CardHeader>
+          <CardContent className="flex flex-col gap-sm">
+            <h3 className="text-lg font-semibold">Featured Suppliers</h3>
+            {tile.suppliers.map((supplier) => (
+              <div key={supplier.id} className="flex flex-row items-center justify-between">
+                <div>{supplier.name}</div>
+                <Link href={`/suppliers/${supplier.handle}`} className="text-sm text-muted-foreground">
+                  <Button variant={'ghost'} size="sm" className="flex items-center gap-xxs">
+                    <LinkIcon className="h-4 w-4" />
+                    <span>{supplier.handle}</span>
+                  </Button>
+                </Link>
+              </div>
+            ))}
+          </CardContent>
+          <CardFooter className="flex flex-row-reverse"></CardFooter>
+        </div>
+      </Area>
     </Section>
   )
 }
