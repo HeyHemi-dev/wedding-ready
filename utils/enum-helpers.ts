1ea688b67169d8b8b700ac2fd076dd5c3870f832
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-14 15:08:07 +1200

    Return the key to make it easy to get the specific enum

--- utils/enum-helpers.ts
+++ utils/enum-helpers.ts
@@ -3,6 +3,7 @@ export function enumToPgEnum<T extends Record<string, any>>(myEnum: T): [T[keyof
 }
 
 interface PrettyEnum {
+  key: string
   value: string
   label: string
 }
@@ -14,8 +15,17 @@ export function valueToPretty(value: string): string {
     .join(' ')
 }
 
+/**
+ * Converts an enum to a array of prettified objects
+ * @example Location -> [{ key: 'BAY_OF_PLENTY', value: 'bay-of-plenty', label: 'Bay of Plenty' }]
+ * @returns PrettyEnum[] with:
+ * key (enum key),
+ * value (enum value),
+ * label (prettified value)
+ */
 export function enumToPretty<T extends { [key: string]: string }>(enumObject: T): PrettyEnum[] {
-  return Object.values(enumObject).map((value) => ({
+  return Object.values(enumObject).map((value, index) => ({
+    key: Object.keys(enumObject)[index],
     value,
     label: valueToPretty(value),
   }))
@@ -25,14 +35,14 @@ export function enumToPretty<T extends { [key: string]: string }>(enumObject: T)
  * Converts an enum key to a URL-friendly parameter
  * @example BAY_OF_PLENTY -> bay-of-plenty
  */
-export function enumLabelToParam(label: string): string {
-  return label.toLowerCase().replace(/_/g, '-')
+export function enumKeyToParam(key: string): string {
+  return key.toLowerCase().replace(/_/g, '-')
 }
 
 /**
  * Converts a URL parameter back to an enum key
  * @example bay-of-plenty -> BAY_OF_PLENTY
  */
-export function paramToEnumLabel<T extends { [K in string]: string }>(param: string, enumObj: T): keyof T {
-  return param.replace(/-/g, '_').toUpperCase() as keyof T & string
+export function paramToEnumKey<T extends { [K in string]: string }>(param: string, enumObj: T): keyof T {
+  return param.replace(/-/g, '_').toUpperCase() as keyof T
 }
