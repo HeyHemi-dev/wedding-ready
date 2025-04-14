d4de4904ec523301fa1264dffd326d4cc8dc68a2
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-15 09:45:25 +1200

    UX improvements

--- app/t/[tileId]/page.tsx
+++ app/t/[tileId]/page.tsx
@@ -1,18 +1,18 @@
 import { QueryClient } from '@tanstack/react-query'
+import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 import { SaveTileButton } from '@/components/tiles/save-button'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Area } from '@/components/ui/area'
+import { Button } from '@/components/ui/button'
 import { Section } from '@/components/ui/section'
+import { Separator } from '@/components/ui/separator'
 import { tileKeys } from '@/hooks/queryKeys'
 import { TileModel } from '@/models/tile'
 import { getAuthenticatedUserId } from '@/utils/auth'
 import { valueToPretty } from '@/utils/enum-helpers'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { Link as LinkIcon } from 'lucide-react'
-import { Area } from '@/components/ui/area'
 
 export default async function TilePage({ params }: { params: Promise<{ tileId: string }> }) {
   const { tileId } = await params
@@ -37,26 +37,32 @@ export default async function TilePage({ params }: { params: Promise<{ tileId: s
           {authUserId && <SaveTileButton tileId={tile.id} authUserId={authUserId} className="absolute right-0 top-0 p-2" />}
         </div>
         <div className="grid grid-rows-[auto_1fr_auto] gap-md">
-          <CardHeader>
+          <div className="flex flex-col gap-xs">
             <h1 className="font-serif text-4xl">{tile.title}</h1>
-            {tile.location && <CardDescription>{valueToPretty(tile.location as string)}</CardDescription>}
-            {tile.description && <CardDescription>{tile.description}</CardDescription>}
-          </CardHeader>
-          <CardContent className="flex flex-col gap-sm">
+            {tile.location && <p className="text-sm text-muted-foreground">{valueToPretty(tile.location as string)}</p>}
+            {tile.description && <p className="text-sm text-muted-foreground">{tile.description}</p>}
+          </div>
+          <div className="flex flex-col gap-sm">
+            <Separator />
             <h3 className="text-lg font-semibold">Featured Suppliers</h3>
             {tile.suppliers.map((supplier) => (
               <div key={supplier.id} className="flex flex-row items-center justify-between">
                 <div>{supplier.name}</div>
                 <Link href={`/suppliers/${supplier.handle}`} className="text-sm text-muted-foreground">
                   <Button variant={'ghost'} size="sm" className="flex items-center gap-xxs">
-                    <LinkIcon className="h-4 w-4" />
                     <span>{supplier.handle}</span>
+                    <ArrowRight className="h-4 w-4" />
                   </Button>
                 </Link>
               </div>
             ))}
-          </CardContent>
-          <CardFooter className="flex flex-row-reverse"></CardFooter>
+            <Button variant={'outline'}>Add Supplier</Button>
+          </div>
+          <div className="flex flex-row-reverse">
+            <Button variant={'link'} size="sm">
+              Report
+            </Button>
+          </div>
         </div>
       </Area>
     </Section>
