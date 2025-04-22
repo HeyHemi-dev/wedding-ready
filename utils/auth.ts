7a6889f46b77e2d59483b69e64e53246b43b80fd
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-22 13:59:37 +1200

    New method for getting the auth user for actions that don't trigger middleware

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,4 +1,5 @@
 import { headers } from 'next/headers'
+import { createClient } from './supabase/server'
 
 export const PROTECTED_PATHS = ['/feed', '/account', '/suppliers/register', '/suppliers/:handle/new']
 
@@ -17,11 +18,29 @@ export const AUTH_HEADER_NAME = 'x-auth-user-id'
  *
  * @returns The authenticated user's id or null if not authenticated
  */
-export async function getAuthenticatedUserId(): Promise<string | null> {
+export async function getAuthUserId(): Promise<string | null> {
   const headersList = await headers()
 
   // userId will a valid string if authHeaderName is set. This is handled in middleware
   const userId = headersList.get(AUTH_HEADER_NAME)
 
   return userId
+}
+
+export async function getAuthUserIdForAction(): Promise<string | null> {
+  const supabase = await createClient()
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser()
+
+  if (error) {
+    throw new Error('Failed to get authenticated user')
+  }
+
+  if (!user) {
+    return null
+  }
+
+  return user.id
 }
