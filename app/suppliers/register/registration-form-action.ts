3d1f6d35cc6e361c2a8cf015908591437c0a0a5e
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-23 19:18:45 +1200

    Check a handle is still avaliable when form is submitted

--- app/suppliers/register/registration-form-action.ts
+++ app/suppliers/register/registration-form-action.ts
@@ -4,6 +4,7 @@ import { SupplierRegistrationForm, supplierRegistrationFormSchema } from '@/app/
 import { supplierActions } from '@/app/_actions/supplier-actions'
 import { getAuthUserIdForAction } from '@/utils/auth'
 import { tryCatch } from '@/utils/try-catch'
+import { SupplierModel } from '@/models/supplier'
 
 export async function registrationFormAction({ data }: { data: SupplierRegistrationForm }): Promise<{ handle: string }> {
   const { success, error, data: validatedData } = supplierRegistrationFormSchema.safeParse(data)
@@ -14,6 +15,11 @@ export async function registrationFormAction({ data }: { data: SupplierRegistrat
   const { data: authUserId, error: authUserIdError } = await tryCatch(getAuthUserIdForAction())
   if (authUserIdError || !authUserId || validatedData.createdByUserId !== authUserId) {
     throw new Error('Unauthorized')
+  }
+
+  const { data: isAvailable, error: isAvailableError } = await tryCatch(SupplierModel.isHandleAvailable({ handle: validatedData.handle }))
+  if (isAvailableError || !isAvailable) {
+    throw new Error(isAvailableError?.message || 'Handle is already taken')
   }
 
   const { data: supplier, error: supplierError } = await tryCatch(supplierActions.register({ supplierRegistrationFormData: validatedData }))
