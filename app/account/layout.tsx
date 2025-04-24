a7460b1add88cf842d80cd8b7ef849b8994fcbd7
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-24 19:17:46 +1200

    Improve UX of account navigation

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -2,30 +2,54 @@ import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 import { Section } from '@/components/ui/section'
+import { User, LucideIcon, Settings, Contact, Bell, Lock } from 'lucide-react'
+import { cn } from '@/utils/shadcn-utils'
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   return (
     <Section>
       <div className="grid grid-cols-1 gap-md md:grid-cols-[minmax(200px,1fr)_3fr]">
-        <nav className="flex flex-col gap-2">
-          <Link href="/account">
-            <Button variant={'ghost'}>Public Profile</Button>
-          </Link>
-          <Link href="/account/account-settings">
-            <Button variant={'ghost'}>Account Settings</Button>
-          </Link>
-          <Link href="/account/link-suppliers">
-            <Button variant={'ghost'}>Linked Suppliers</Button>
-          </Link>
-          <Link href="/account">
-            <Button variant={'ghost'}>Notifications</Button>
-          </Link>
-          <Link href="/account">
-            <Button variant={'ghost'}>Privacy</Button>
-          </Link>
+        <nav className="grid auto-rows-fr self-start">
+          <AccountNavLink href="/account" title="Profile" description="Personalize your name, avatar, bio and social links." Icon={User} />
+          <AccountNavLink
+            href="/account/account-settings"
+            title="Account & Security"
+            description="Update your email, password and user handle."
+            Icon={Settings}
+          />
+          <AccountNavLink href="/account/manage-suppliers" title="My Suppliers" description="Add, edit or remove the suppliers you manage." Icon={Contact} />
+          <AccountNavLink href="/account/notifications" title="Notifications" description="Choose which alerts you receive and how." Icon={Bell} />
+          <AccountNavLink href="/account/privacy" title="Privacy" description="Control your data, visibility and permissions." Icon={Lock} />
         </nav>
         {children}
       </div>
     </Section>
+  )
+}
+
+function AccountNavLink({
+  href,
+  title,
+  description,
+  Icon,
+  isActive = false,
+}: {
+  href: string
+  title: string
+  description: string
+  Icon: LucideIcon
+  isActive?: boolean
+}) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'grid h-auto max-w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr] gap-x-xs gap-y-xxs rounded-md px-sm py-sm hover:bg-accent',
+        isActive && 'bg-accent'
+      )}>
+      <Icon className="row-span-2 self-start text-muted-foreground" size={20} />
+      <p className="text-sm">{title}</p>
+      <p className="text-pretty text-xs text-muted-foreground">{description}</p>
+    </Link>
   )
 }
