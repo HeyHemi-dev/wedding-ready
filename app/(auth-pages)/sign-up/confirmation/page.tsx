209c2d35a5c14f4d10eca757c4b4a9aa4ab71569
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-24 16:05:03 +1200

    Improve confirmation screen UX

--- app/(auth-pages)/sign-up/confirmation/page.tsx
+++ app/(auth-pages)/sign-up/confirmation/page.tsx
@@ -3,32 +3,36 @@ import { getAuthUserId } from '@/utils/auth'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
-export default async function SignupConfirmation() {
-  // If user is already logged in, they don't need to be here.
-  const authUserId = await getAuthUserId()
-
-  if (authUserId) {
-    redirect('/feed')
-  }
-
+export default function SignUpConfirmation() {
   return (
     <>
-      <div className="grid gap-xxs text-center">
-        <h1 className="text-2xl font-medium">Thanks for signing up!</h1>
-        <p className="text text-sm text-foreground">We've sent you an email to confirm your account. Please check your email and click the link to continue.</p>
+      <div className="grid gap-xs text-center">
+        <h1 className="font-serif text-4xl font-medium">Thanks for signing up!</h1>
+        <p className="text text-sm text-foreground">We've sent you an email to confirm your account.</p>
       </div>
       <div className="flex flex-col gap-sm">
         <Button variant="outline" asChild>
-          <Link href="https://mail.google.com">Open Gmail</Link>
+          <Link href="https://mail.google.com" target="_blank">
+            Open Gmail
+          </Link>
         </Button>
         <Button variant="outline" asChild>
-          <Link href="https://outlook.live.com/mail/">Open Outlook</Link>
+          <Link href="https://outlook.live.com/mail/" target="_blank">
+            Open Outlook
+          </Link>
         </Button>
         <Button variant="outline" asChild>
-          <Link href="https://outlook.office.com/mail/">Open Office 365</Link>
+          <Link href="https://outlook.office.com/mail/" target="_blank">
+            Open Office 365
+          </Link>
         </Button>
         <Button variant="outline" asChild>
-          <Link href="https://icloud.com">Open iCloud Mail</Link>
+          <Link href="https://icloud.com/mail/" target="_blank">
+            Open iCloud Mail
+          </Link>
+        </Button>
+        <Button variant="ghost" asChild>
+          <Link href="/feed">Skip for now</Link>
         </Button>
       </div>
     </>
