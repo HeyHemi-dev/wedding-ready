8b2d49863f2b44f5f8832ae00271594224890617
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-25 09:10:21 +1200

    Check auth in the action and set default values for uncontrolled form

--- app/account/update-profile-form-action.ts
+++ app/account/update-profile-form-action.ts
@@ -5,11 +5,17 @@ import { revalidateTag } from 'next/cache'
 import { UserUpdateForm, userUpdateFormSchema } from '@/app/_types/validation-schema'
 import { tryCatch } from '@/utils/try-catch'
 import { userActions } from '../_actions/user-actions'
+import { getAuthUserIdFromSupabase } from '@/utils/auth'
 
 export async function updateProfileFormAction({ data }: { data: UserUpdateForm }): Promise<UserUpdateForm> {
   const { success, error: parseError, data: validatedData } = userUpdateFormSchema.safeParse(data)
   if (!success || parseError) {
     throw new Error(JSON.stringify(parseError?.flatten().fieldErrors))
+  }
+
+  const { data: authUserId, error: authUserIdError } = await tryCatch(getAuthUserIdFromSupabase())
+  if (authUserIdError || !authUserId || validatedData.id !== authUserId) {
+    throw new Error('Unauthorized')
   }
 
   const { data: user, error: updateError } = await tryCatch(userActions.updateProfile(validatedData))
@@ -23,10 +29,10 @@ export async function updateProfileFormAction({ data }: { data: UserUpdateForm }
   return {
     id: user.id,
     displayName: user.displayName,
-    bio: user.bio ?? undefined,
-    avatarUrl: user.avatarUrl ?? undefined,
-    instagramUrl: user.instagramUrl ?? undefined,
-    tiktokUrl: user.tiktokUrl ?? undefined,
-    websiteUrl: user.websiteUrl ?? undefined,
+    bio: user.bio ?? '',
+    avatarUrl: user.avatarUrl ?? '',
+    instagramUrl: user.instagramUrl ?? '',
+    tiktokUrl: user.tiktokUrl ?? '',
+    websiteUrl: user.websiteUrl ?? '',
   }
 }
