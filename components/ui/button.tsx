74c5e4ae0b3373b7826d0e7de589c2304c09dc24
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-04-29 08:59:54 +1200

    Apply theme to buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,11 +10,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input hover:bg-accent hover:text-accent-foreground',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        outline: 'border border-white border-input hover:bg-primary/80 text-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-primary/80 hover:text-primary-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
