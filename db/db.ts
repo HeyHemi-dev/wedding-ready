df169a7bab3c14d71ea03e32d454e098ffad3144
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-08 11:01:31 +1200

    Load env correctly in local dev

--- db/db.ts
+++ db/db.ts
@@ -1,8 +1,17 @@
+import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+
 import * as schema from './schema'
 
+// Load .env.local for local development or Vercel development environments
+// Vercel automatically sets env vars for preview/production
+if (!process.env.VERCEL_ENV || process.env.VERCEL_ENV === 'development') {
+  config({ path: '.env.local', override: true })
+}
+
 const connectionString = process.env.DATABASE_URL!
+console.log('Attempting to connect with:', connectionString)
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false })
