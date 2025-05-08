d83cec2b6aed60467abc52fa40687a413346ecb7
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-09 11:07:29 +1200

    Make tile detail page match designs

--- app/t/[tileId]/page.tsx
+++ app/t/[tileId]/page.tsx
@@ -1,5 +1,5 @@
 import { QueryClient } from '@tanstack/react-query'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Plus } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
@@ -30,41 +30,94 @@ export default async function TilePage({ params }: { params: Promise<{ tileId: s
 
   return (
     <Section className="min-h-svh-minus-header">
-      <Area className="grid grid-cols-1 gap-md md:grid-cols-2">
-        <div className="relative bg-muted">
+      <div className="grid grid-cols-1 gap-area md:grid-cols-2">
+        <Area className="relative overflow-clip rounded-area p-contour">
           <Image src={tile.imagePath} alt={tile.title ?? ''} fill sizes="(max-width: 768px) 100vw, 50vw" className="object-contain" />
-
           {authUserId && <SaveTileButton tileId={tile.id} authUserId={authUserId} className="absolute right-0 top-0 p-2" />}
-        </div>
-        <div className="grid grid-rows-[auto_1fr_auto] gap-md">
-          <div className="flex flex-col gap-xs">
-            <h1 className="font-serif text-4xl">{tile.title}</h1>
-            {tile.location && <p className="text-sm text-muted-foreground">{valueToPretty(tile.location as string)}</p>}
-            {tile.description && <p className="text-sm text-muted-foreground">{tile.description}</p>}
+        </Area>
+        <Area className="grid grid-rows-[auto_1fr_auto] gap-acquaintance bg-transparent">
+          <div className="flex flex-col gap-bff">
+            <div className="ui-small flex flex-row gap-sibling text-muted-foreground">
+              <span>{formatRelativeDate(tile.createdAt)}</span>
+              <span>•</span>
+              {tile.location && <span>{valueToPretty(tile.location as string)}</span>}
+            </div>
+            <h1 className="heading-lg">{tile.title ?? 'Untitled'}</h1>
+            {tile.description && <p className="text-muted-foreground">{tile.description}</p>}
           </div>
-          <div className="flex flex-col gap-sm">
-            <Separator />
-            <h3 className="text-lg font-semibold">Featured Suppliers</h3>
+          <div className="flex flex-col gap-bff">
+            <div className="flex items-center justify-between gap-friend">
+              <h3 className="ui-s1">Supplier Credits</h3>
+              <Button variant={'ghost'} size="sm" className="flex items-center gap-spouse">
+                <Plus className="h-4 w-4" />
+                <span>Add Supplier</span>
+              </Button>
+            </div>
             {tile.suppliers.map((supplier) => (
-              <div key={supplier.id} className="flex flex-row items-center justify-between">
-                <div>{supplier.name}</div>
-                <Link href={`/suppliers/${supplier.handle}`} className="text-sm text-muted-foreground">
-                  <Button variant={'ghost'} size="sm" className="flex items-center gap-xxs">
-                    <span>{supplier.handle}</span>
-                    <ArrowRight className="h-4 w-4" />
-                  </Button>
-                </Link>
-              </div>
+              <SupplierCredit key={supplier.id} name={supplier.name} contribution={'Contribution description'} href={`/suppliers/${supplier.handle}`} />
             ))}
-            <Button variant={'outline'}>Add Supplier</Button>
           </div>
           <div className="flex flex-row-reverse">
             <Button variant={'link'} size="sm">
               Report
             </Button>
           </div>
-        </div>
-      </Area>
+        </Area>
+      </div>
     </Section>
   )
+}
+
+function SupplierCredit({ name, contribution, href }: { name: string; contribution: string; href: string }) {
+  return (
+    <div className="flex flex-row items-center justify-between gap-bff">
+      <div className="flex gap-sibling">
+        <Link href={href} className="ui-small">
+          {name}
+        </Link>
+        <span className="ui-small text-muted-foreground">{contribution}</span>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * Given a past Date, returns one of:
+ *  - "less than 1 minute ago"
+ *  - "about 1 minute ago"
+ *  - "about X minutes ago"
+ *  - "about 1 hour ago"
+ *  - "about X hours ago"
+ *  - "about 1 day ago"
+ *  - "about X days ago"
+ */
+export function formatRelativeDate(date: Date): string {
+  const now = Date.now()
+
+  const diffSec = Math.floor((now - date.getTime()) / 1000)
+  if (diffSec < 60) {
+    return 'less than 1 minute ago'
+  }
+  if (diffSec < 120) {
+    return 'about 1 minute ago'
+  }
+
+  const diffMin = Math.floor(diffSec / 60)
+  if (diffMin < 60) {
+    return `about ${diffMin} minutes ago`
+  }
+  if (diffMin < 120) {
+    return 'about 1 hour ago'
+  }
+
+  const diffHr = Math.floor(diffMin / 60)
+  if (diffHr < 24) {
+    return `about ${diffHr} hours ago`
+  }
+  if (diffHr < 48) {
+    return 'about 1 day ago'
+  }
+
+  const diffDay = Math.floor(diffHr / 24)
+  return `about ${diffDay} days ago`
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,21 +6,21 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/utils/shadcn-utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded  ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'border border-white border-input hover:bg-primary/80 text-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-primary/80 hover:text-primary-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default: 'ui-small-s1 bg-primary text-primary-foreground hover:bg-primary/80',
+        destructive: 'ui-small-s1 bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        outline: 'ui-small-s1 border border-white border-input hover:bg-primary/80 text-foreground',
+        secondary: 'ui-small-s1 bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'ui-small-s1 hover:bg-primary/80 hover:text-primary-foreground',
+        link: 'text-primary-foreground underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        sm: 'h-9 rounded px-3',
+        lg: 'h-11 rounded px-8',
         icon: 'h-10 w-10',
       },
     },
