cc93ffe90f47c533806ec2559a672513d4b7dab9
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-12 15:33:32 +1200

    Add service descriptions

---
+++ public/service-descriptions.ts
@@ -0,0 +1,122 @@
+import { Service } from '@/db/constants'
+
+export const serviceDescriptions = {
+  [Service.ACCOMODATION]: {
+    title: 'New Zealand Wedding Accommodation',
+    description:
+      'If you’re bringing people together from all over, accommodation can make or break the weekend. Luckily, Aotearoa does hospitality better than most. Think boutique cabins, lakeside lodges, and family-run motels where the kettle’s always on. These places aren’t just somewhere to sleep—they help set the tone for a relaxed and memorable weekend.',
+  },
+  [Service.BAND]: {
+    title: 'New Zealand Wedding Bands & DJs',
+    description: '',
+  },
+  [Service.BEAUTY]: {
+    title: 'New Zealand Wedding Beauty Services',
+    description: '',
+  },
+  [Service.BRIDAL_ACCESSORY]: {
+    title: 'New Zealand Bridal Jewellery & Accessories',
+    description:
+      'Jewellery, veils, capes, headpieces—these are the little details that bring the whole look together. Whether you’re making a statement or keeping things simple, the right piece should feel like an extension of you.',
+  },
+  [Service.BRIDAL_WEAR]: {
+    title: 'New Zealand Wedding Dresses',
+    description:
+      'Whether you’re after minimalist tailoring, vintage glamour, or something entirely off-script, Aotearoa’s bridal scene has the range. These bridalwear specialists know how to listen, offer honest direction, and guide you toward something that feels like you—comfortable, expressive, and right for the day you’re planning.',
+  },
+  [Service.BRIDESMAIDS_WEAR]: {
+    title: 'New Zealand Bridesmaid Dresses',
+    description: '',
+  },
+  [Service.CAKE]: {
+    title: 'New Zealand Wedding Cakes',
+    description:
+      'From three-tier showstoppers to towers of doughnuts or cheese wheels; cake is whatever you want it to be. Whether you’re after tradition or a modern twist, there is a baker here to help you sweeten the day.',
+  },
+  [Service.CATERER]: {
+    title: 'New Zealand Wedding Caterers',
+    description:
+      'Aotearoa’s food scene is a reflection of its rich, multicultural roots—grounded in the land, shaped by migration, and always evolving. From pāua fritters to biryani bowls, local caterers bring together flavours from across the globe with a distinct Kiwi ease. Expect menus built around local produce, relaxed service, and dishes that invite people to gather, share, and celebrate.',
+  },
+  [Service.CELEBRANT]: {
+    title: 'New Zealand Wedding Celebrants',
+    description:
+      'The right celebrant can change everything. These are the people who’ll hold the space, set the tone, and guide you through your most personal moments. Here, you’ll find celebrants who are inclusive, adaptable, and genuinely invested in your story.',
+  },
+  [Service.ENTERTAINMENT]: {
+    title: 'New Zealand Wedding Entertainment Services',
+    description:
+      'Looking for something a bit unexpected? From lawn games to live painting —entertainment goes beyond the dance floor. These suppliers can help you set the tone, break the ice, or just keep the fun rolling.',
+  },
+  [Service.FLORIST]: {
+    title: 'New Zealand Wedding Florists',
+    description: '',
+  },
+  [Service.HAIR]: {
+    title: 'New Zealand Wedding Hair Stylists',
+    description: '',
+  },
+  [Service.HIRE]: {
+    title: 'New Zealand Wedding Hire Services',
+    description: '',
+  },
+  [Service.MAKEUP]: {
+    title: 'New Zealand Wedding Makeup Artists',
+    description: '',
+  },
+  [Service.MC]: {
+    title: 'New Zealand Professional Wedding MCs',
+    description:
+      'A good MC knows when to speak up and when to get out of the way. These pros can wrangle a rowdy crowd, smooth out the transitions, and keep the night on track—without becoming the centre of attention themselves',
+  },
+  [Service.MENSWEAR]: {
+    title: 'New Zealand Wedding Suits and Menswear',
+    description:
+      'Whether it’s a classic suit or something a bit more coastal-casual, the menswear scene here knows how to do both sharp and relaxed. Natural fibres, good fits, and nothing too try-hard. Here, you’ll find tailors who can help you walk that line in style.',
+  },
+  [Service.MENSWEAR_ACCESSORY]: {
+    title: 'New Zealand Wedding Menswear Accessories',
+    description:
+      'Ties, cufflinks, pāua shell pins, or socks that make you laugh; accessories offer a chance to show a bit of personality. Whether you’re honouring heritage, colour-coordinating, or just having fun, these details help pull the whole outfit into place.',
+  },
+  [Service.PHOTOGRAPHER]: {
+    title: 'New Zealand Wedding Photographers',
+    description: '',
+  },
+  [Service.PLANNER]: {
+    title: 'New Zealand Wedding Planners',
+    description: '',
+  },
+  [Service.RINGS]: {
+    title: 'New Zealand Wedding Rings',
+    description:
+      'Worn every day, rings should feel like you. From classic gold bands to handcrafted taonga, there’s something meaningful about choosing a piece that reflects your story and values. These makers and jewellers can help you find the right fit.',
+  },
+  [Service.STATIONERY]: {
+    title: 'New Zealand Wedding Stationery',
+    description: '',
+  },
+  [Service.STYLIST]: {
+    title: 'New Zealand Wedding Stylists',
+    description:
+      'Stylists don’t just make things look pretty—they create an atmosphere. They’ll think about lighting, textures, flow, and how it all ties together. Whether you’re going for refined or rustic, these creatives bring an editor’s eye and a practical mind.',
+  },
+  [Service.SUPPORT]: {
+    title: 'New Zealand Wedding Support Services',
+    description:
+      'Behind every good wedding is a crew that keeps things ticking. Whether it’s a dog sitting service, setup team, or on-the-day coordinator —this is where you’ll find the quiet MVPs who make the day seamless.',
+  },
+  [Service.TRANSPORT]: {
+    title: 'New Zealand Wedding Transportation Services',
+    description: '',
+  },
+  [Service.VENUE]: {
+    title: 'New Zealand Wedding Venues',
+    description:
+      'Kiwi weddings rarely stick to one mould, and these venues don’t either. From beachfront baches and vineyard barns to sleek city spots and high-country lodges, there’s a space here for every kind of celebration—big, small, traditional, or offbeat. Whether you’re after something laid-back or luxurious, what you’ll find is choice, character, and often, a view.',
+  },
+  [Service.VIDEOGRAPHER]: {
+    title: 'New Zealand Wedding Videographers',
+    description: '',
+  },
+} as const
