551a27d3e37d4289b97c0bd06e6093282c88053d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-14 16:07:18 +1200

    use pathname passed in from client for redirect

--- components/navigation/signout-form-action.tsx
+++ components/navigation/signout-form-action.tsx
@@ -4,15 +4,13 @@ import { revalidateTag } from 'next/cache'
 import { headers } from 'next/headers'
 
 import { authActions } from '@/app/_actions/auth-actions'
-
 import { isProtectedPath } from '@/utils/auth'
 import { tryCatch } from '@/utils/try-catch'
+
 
 export async function SignOutFormAction({ pathname }: { pathname: string }): Promise<{ redirectTo: string }> {
   const headersList = await headers()
   const userId = headersList.get('x-auth-user-id')
-  const referer = headersList.get('referer') || '/'
-  const url = new URL(referer)
 
   const { error } = await tryCatch(authActions.signOut())
 
@@ -24,8 +22,5 @@ export async function SignOutFormAction({ pathname }: { pathname: string }): Pro
     revalidateTag(`user-${userId}`)
   }
 
-  const redirectTo = isProtectedPath(url.pathname) ? '/sign-in' : referer
-  console.log({ referer, redirectTo })
-
-  return { redirectTo }
+  return { redirectTo: isProtectedPath(pathname) ? '/sign-in' : pathname }
 }
