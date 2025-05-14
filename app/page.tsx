8190bdda89bba74c1fe1cbe49f37f85e26b06aa6
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-14 15:46:20 +1200

    Use semantic classes for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { Section } from '@/components/ui/section'
 
 import { StackingCardsContainer, StackingCardContent } from './stacking-cards'
 
-
 const cards: StackingCardContent[] = [
   {
     title: 'Explore inspiration that fits your wedding.',
@@ -47,60 +46,58 @@ const cards: StackingCardContent[] = [
 export default function Home() {
   return (
     <>
-      <Section className="min-h-svh-minus-header pt-md">
-        <div className="gap-area grid grid-cols-3 grid-rows-4">
-          <Area className="col-span-2 row-span-full grid place-items-center">
-            <div className="flex flex-col gap-md pr-xxl">
-              <h1 className="text-balance font-serif text-6xl">Wedding inspiration you can actually book.</h1>
+      <Section className="min-h-svh-minus-header pt-0">
+        <div className="grid grid-cols-3 grid-rows-4 gap-area">
+          <Area className="col-span-2 row-span-full grid place-content-center gap-acquaintance">
+            <div className="flex flex-col gap-sibling pr-xxl">
+              <h1 className="heading-2xl">Wedding inspiration you can actually book.</h1>
               <p className="text-pretty text-xl">Explore local ideas, save what you love, and connect with real NZ suppliers—all for free.</p>
-              <div className="flex gap-xs pt-md">
-                <Button size={'lg'} asChild>
-                  <Link href="/sign-up" className="flex items-center gap-xs">
-                    <span>Sign up now</span>
-                    <ArrowRight />
-                  </Link>
-                </Button>
-                <Button variant={'outline'} size={'lg'} asChild>
-                  <Link href="/feed">
-                    <span className="font-semibold">Start exploring</span>
-                  </Link>
-                </Button>
-              </div>
+            </div>
+            <div className="flex gap-sibling">
+              <Button size={'lg'} asChild>
+                <Link href="/sign-up" className="flex items-center gap-spouse">
+                  <span>Sign up now</span>
+                  <ArrowRight />
+                </Link>
+              </Button>
+              <Button variant={'ghost'} size={'lg'} asChild>
+                <Link href="/feed">
+                  <span className="font-semibold">Start exploring</span>
+                </Link>
+              </Button>
             </div>
           </Area>
           <Area className="relative col-span-1 row-span-3 overflow-hidden">
             <Image className="object-cover" src="/assets/home-hero.jpg" alt="Couple just married celebrating with confetti" fill sizes="100vw" />
           </Area>
-          <Area className="col-span-1 row-span-1">
-            <div className="flex flex-col gap-md"></div>
-          </Area>
+          <Area className="col-span-1 row-span-1"></Area>
         </div>
       </Section>
 
       <StackingCardsContainer cards={cards} />
 
       <Section>
-        <div className="grid min-h-[33svh] grid-cols-3 grid-rows-1 gap-md">
+        <div className="grid min-h-[33svh] grid-cols-3 grid-rows-1 gap-area">
           <Area className="relative col-span-1 row-span-full overflow-hidden">
             <Image className="object-cover" src="/assets/home-supplier2.jpg" alt="Indian wedding couple exchanging garlands" fill sizes="33vw" />
           </Area>
-          <Area className="col-span-2 row-span-full grid place-items-center">
-            <div className="flex flex-col gap-md pr-xxl">
-              <h2 className="text-balance font-serif text-4xl">Are you a wedding supplier?</h2>
+          <Area className="col-span-2 row-span-full grid place-content-center gap-friend">
+            <div className="flex flex-col gap-sibling pr-xxl">
+              <h2 className="heading-lg">Are you a wedding supplier?</h2>
               <p className="text-pretty">
                 Reach more couples, showcase your work, and get discovered on WeddingReady. It&apos;s free to join, and only takes a few minutes to set up.
               </p>
-              <div className="flex gap-xs pt-md">
-                <Button asChild size="lg">
-                  <Link href="/suppliers/join" className="flex items-center gap-xs">
-                    <span>Join as a supplier</span>
-                    <ArrowRight />
-                  </Link>
-                </Button>
-                <Button asChild size="lg" variant={'outline'}>
-                  <Link href="/suppliers/join">Learn more</Link>
-                </Button>
-              </div>
+            </div>
+            <div className="flex gap-sibling">
+              <Button asChild size="lg">
+                <Link href="/suppliers/join" className="flex items-center gap-spouse">
+                  <span>Join as a supplier</span>
+                  <ArrowRight />
+                </Link>
+              </Button>
+              <Button asChild size="lg" variant={'ghost'}>
+                <Link href="/suppliers/join">Learn more</Link>
+              </Button>
             </div>
           </Area>
         </div>
