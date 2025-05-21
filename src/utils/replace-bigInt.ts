1056fb6ecbf4b1519db0fff8a18ecf1aa4a9ede8
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-05-21 20:53:33 +1200

    Remove unused function

--- src/utils/replace-bigInt.ts
+++
@@ -1,9 +0,0 @@
-// Convert BigInt to number
-const replaceBigInt = (key: string, value: any) => {
-  if (typeof value === 'bigint') {
-    return Number(value)
-  }
-  return value
-}
-
-export default replaceBigInt
