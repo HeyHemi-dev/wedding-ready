fc9f49fd391c9edc3a05b077e84dff1e47c1857d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-06-02 21:08:29 +1200

    Use getAuthId function because its more DRY

--- src/components/navigation/signout-form-action.tsx
+++ src/components/navigation/signout-form-action.tsx
@@ -1,17 +1,15 @@
 'use server'
 
 import { revalidateTag } from 'next/cache'
-import { headers } from 'next/headers'
 
 import { tags } from '@/app/_types/tags'
 import { authOperations } from '@/operations/auth-operations'
-import { isProtectedPath } from '@/utils/auth'
+import { getAuthUserId, isProtectedPath } from '@/utils/auth'
 import { createClient } from '@/utils/supabase/server'
 import { tryCatch } from '@/utils/try-catch'
 
 export async function SignOutFormAction({ pathname }: { pathname: string }): Promise<{ redirectTo: string }> {
-  const headersList = await headers()
-  const userId = headersList.get('x-auth-user-id')
+  const authUserId = await getAuthUserId()
 
   const supabase = await createClient()
   const { error } = await tryCatch(authOperations.signOut({ supabaseClient: supabase }))
@@ -20,8 +18,8 @@ export async function SignOutFormAction({ pathname }: { pathname: string }): Pro
     throw new Error('Failed to sign out')
   }
 
-  if (userId) {
-    revalidateTag(tags.currentUser(userId))
+  if (authUserId) {
+    revalidateTag(tags.currentUser(authUserId))
   }
 
   // Safe even if pathname is manipulated client-side.
