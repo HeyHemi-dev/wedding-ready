cf6bdb1ac3823ccd25375023330d273e81d4840b
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-07-27 18:49:18 +1200

    Add TileSupplierModel for managing tile-supplier relationships

---
+++ src/models/tileSupplier.ts
@@ -0,0 +1,37 @@
+import { db } from '@/db/connection'
+import * as s from '@/db/schema'
+import type * as t from './types'
+import { eq } from 'drizzle-orm'
+import { TileModel } from './tile'
+import { SupplierModel } from './supplier'
+
+export const TileSupplierModel = {
+  getCreditsByTileId,
+  createRaw,
+}
+
+async function getCreditsByTileId(tileId: string): Promise<t.TileCredit[]> {
+  const tileCredits = await db
+    .select({
+      ...s.tileSupplierColumns,
+      supplier: s.suppliers,
+    })
+    .from(s.tileSuppliers)
+    .innerJoin(s.suppliers, eq(s.tileSuppliers.supplierId, s.suppliers.id))
+    .where(eq(s.tileSuppliers.tileId, tileId))
+
+  return tileCredits
+}
+
+async function createRaw(tileSupplierRawData: t.InsertTileSupplierRaw): Promise<t.TileSupplierRaw> {
+  const tile = await TileModel.getRawById(tileSupplierRawData.tileId)
+  const supplier = await SupplierModel.getRawById(tileSupplierRawData.supplierId)
+
+  if (!tile || !supplier) {
+    throw new Error('Tile or supplier not found')
+  }
+
+  const tileSuppliers = await db.insert(s.tileSuppliers).values(tileSupplierRawData).returning()
+
+  return tileSuppliers[0]
+}
