2b5e39422a08cd1a8534c9d6283e2e629d9f54e8
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-07-27 18:49:09 +1200

    Add SavedTilesModel for managing saved tile functionality

---
+++ src/models/savedTiles.ts
@@ -0,0 +1,47 @@
+import { eq, and, inArray } from 'drizzle-orm'
+import * as s from '@/db/schema'
+import * as t from '@/models/types'
+import { db } from '@/db/connection'
+
+export const SavedTilesModel = {
+  getSavedTileRaw,
+  getSavedTilesRaw,
+  upsertSavedTileRaw,
+}
+
+async function getSavedTileRaw(tileId: string, userId: string): Promise<t.SavedTileRaw | null> {
+  const savedTiles = await db
+    .select()
+    .from(s.savedTiles)
+    .where(and(eq(s.savedTiles.tileId, tileId), eq(s.savedTiles.userId, userId)))
+    .limit(1)
+
+  return savedTiles.length ? savedTiles[0] : null
+}
+
+async function getSavedTilesRaw(tileIds: string[], userId: string): Promise<t.SavedTileRaw[]> {
+  const savedTiles = await db
+    .select()
+    .from(s.savedTiles)
+    .where(and(eq(s.savedTiles.userId, userId), inArray(s.savedTiles.tileId, tileIds)))
+  return savedTiles
+}
+
+/**
+ * lets a user save/unsave a tile by upserting the saved tile relationship.
+ * @returns The updated saved status of the tile
+ */
+async function upsertSavedTileRaw(savedTileData: t.InsertSavedTileRaw): Promise<t.SavedTileRaw> {
+  const savedTiles = await db
+    .insert(s.savedTiles)
+    .values(savedTileData)
+    .onConflictDoUpdate({
+      target: [s.savedTiles.tileId, s.savedTiles.userId],
+      set: {
+        isSaved: savedTileData.isSaved,
+      },
+    })
+    .returning()
+
+  return savedTiles[0]
+}
