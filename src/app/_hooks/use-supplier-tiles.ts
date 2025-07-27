7a82c3e6b5b85515b9b99cf135a90a48e93ee88d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-07-28 07:45:08 +1200

    Use root relative imports

--- src/app/_hooks/use-supplier-tiles.ts
+++ src/app/_hooks/use-supplier-tiles.ts
@@ -8,7 +8,7 @@ import { buildQueryParams } from '@/utils/api-helpers'
 import { DEFAULT_STALE_TIME } from '@/utils/constants'
 import { tryCatchFetch } from '@/utils/try-catch'
 
-import { setTilesSaveStateCache } from './use-tile-saved-state'
+import { setTilesSaveStateCache } from '@/app/_hooks/use-tile-saved-state'
 
 export function useSupplierTiles({ supplierId, authUserId }: { supplierId: string; authUserId: string | null }) {
   const queryClient = useQueryClient()
