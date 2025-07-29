4fa0cfb49d78439f543add5aba9035841c3e06e5
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-07-30 09:05:04 +1200

    Refactor useTileCredit hook to improve data fetching and mutation handling

--- src/app/_hooks/use-tile-credit.ts
+++ src/app/_hooks/use-tile-credit.ts
@@ -4,13 +4,11 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
 import { tileKeys } from '@/app/_types/queryKeys'
-import {
-  TileCreditGetResponseBody,
-  TileCreditPostRequestBody,
-  TileCreditPostResponseBody,
-} from '@/app/api/tiles/[tileId]/credits/route'
+import { TileCreditGetResponseBody, TileCreditPostRequestBody, TileCreditPostResponseBody } from '@/app/api/tiles/[tileId]/credits/route'
 import { DEFAULT_STALE_TIME } from '@/utils/constants'
 import { tryCatchFetch } from '@/utils/try-catch'
+import { TileCredit } from '../_types/tiles'
+import { TileCreditForm } from '../_types/validation-schema'
 
 export function useTileCredit(tileId: string) {
   const queryClient = useQueryClient()
@@ -18,11 +16,15 @@ export function useTileCredit(tileId: string) {
   const creditsQuery = useQuery({
     queryKey: tileKeys.credits(tileId),
     queryFn: () => fetchCredits(tileId),
-    staleTime: DEFAULT_STALE_TIME,
+    initialData: () => queryClient.getQueryData(tileKeys.credits(tileId)),
+    staleTime: Infinity,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
   })
 
-  const mutation = useMutation({
-    mutationFn: (data: TileCreditPostRequestBody) => postCredit(tileId, data),
+  const add = useMutation({
+    mutationFn: (tileCreditForm: TileCreditForm) => postCredit(tileId, tileCreditForm),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: tileKeys.credits(tileId) })
     },
@@ -31,7 +33,7 @@ export function useTileCredit(tileId: string) {
     },
   })
 
-  return { ...creditsQuery, addCredit: mutation.mutateAsync }
+  return { ...creditsQuery, addCredit: add.mutateAsync }
 }
 
 async function fetchCredits(tileId: string): Promise<TileCreditGetResponseBody> {
@@ -42,17 +44,15 @@ async function fetchCredits(tileId: string): Promise<TileCreditGetResponseBody> 
   return data
 }
 
-async function postCredit(
-  tileId: string,
-  data: TileCreditPostRequestBody
-): Promise<TileCreditPostResponseBody> {
-  const { data: result, error } = await tryCatchFetch<TileCreditPostResponseBody>(`/api/tiles/${tileId}/credits`, {
+async function postCredit(tileId: string, tileCreditForm: TileCreditForm): Promise<TileCreditPostResponseBody> {
+  const tileCreditRequestBody: TileCreditPostRequestBody = { ...tileCreditForm }
+
+  const { data, error } = await tryCatchFetch<TileCreditPostResponseBody>(`/api/tiles/${tileId}/credits`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
+    body: JSON.stringify(tileCreditRequestBody),
   })
   if (error) {
     throw error
   }
-  return result
+  return data
 }
