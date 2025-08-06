1a8db64b3c1c5f67ad400da2986096b141f1cb0e
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-08-07 09:38:22 +1200

    Add AddCreditButton component to Kitchensink page

--- src/app/kitchensink/page.tsx
+++ src/app/kitchensink/page.tsx
@@ -1,14 +1,28 @@
+'use client'
+
+import { useRef } from 'react'
+
 import { TileCreditForm } from '@/components/tiles/tile-credit-form'
 import { Area } from '@/components/ui/area'
+import { AddCreditButton } from '@/components/tiles/add-credit-button'
 import { Section } from '@/components/ui/section'
 
 export default function KitchensinkPage() {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
   return (
-    <Section>
-      <Area className="grid gap-friend">
-        <h1>Kitchensink</h1>
-        <TileCreditForm tileId="123" />
-      </Area>
-    </Section>
+    <>
+      <Section>
+        <Area className="grid gap-friend">
+          <h1>Kitchensink</h1>
+          <TileCreditForm tileId="123" />
+        </Area>
+      </Section>
+      <Section>
+        <Area className="grid gap-friend">
+          <AddCreditButton tileId="123" />
+        </Area>
+      </Section>
+    </>
   )
 }
