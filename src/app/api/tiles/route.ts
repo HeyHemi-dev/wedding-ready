a2da67f07d2fbdeb66dd7fa16dba6e6b77e0b515
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-08-31 14:42:38 +1200

    Refactor tile creation endpoint to utilize TileCreate type and enhance error handling with ROUTE_ERROR

--- src/app/api/tiles/route.ts
+++ src/app/api/tiles/route.ts
@@ -4,10 +4,11 @@ import * as t from '@/models/types'
 import { tileOperations } from '@/operations/tile-operations'
 import { getAuthUserId } from '@/utils/auth'
 import { Supplier } from '@/app/_types/suppliers'
+import { TileCreate, tileCreateSchema } from '@/app/_types/validation-schema'
+import { tryCatch } from '@/utils/try-catch'
+import { ROUTE_ERROR } from '@/app/_types/errors'
 
-export interface tileNewRequestBody extends t.InsertTileRaw {
-  suppliers: Supplier[]
-}
+export type tileNewRequestBody = TileCreate
 
 export type tileNewResponseBody = { id: string }
 
@@ -15,14 +16,17 @@ export type tileNewResponseBody = { id: string }
 export async function POST(req: Request): Promise<NextResponse> {
   const body = (await req.json()) as tileNewRequestBody
 
-  const authUserId = await getAuthUserId()
-  if (!authUserId || authUserId !== body.createdByUserId) {
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+  const { data: validatedBody, error } = tileCreateSchema.safeParse(body)
+  if (error) {
+    return ROUTE_ERROR.INVALID_REQUEST()
   }
 
-  const { suppliers, ...rest } = body
+  const authUserId = await getAuthUserId()
+  if (!authUserId || authUserId !== body.createdByUserId) {
+    return ROUTE_ERROR.UNAUTHORIZED()
+  }
 
-  const tile = await tileOperations.createForSupplier({ InsertTileRawData: rest, supplierIds: suppliers.map((s) => s.id) })
+  const tile = await tileOperations.createForSupplier(validatedBody)
 
   return NextResponse.json({ id: tile.id })
 }
