78d5e903ef24299fd58c098c557b7c247886c458
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-03 09:25:37 +1300

    Add UpdateSupplierForm component for editing supplier details

---
+++ src/app/account/manage-suppliers/[handle]/update-supplier-form.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { FormFieldItem } from '@/components/form/field'
+import { SubmitButton } from '@/components/submit-button'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/utils/shadcn-utils'
+
+import { SupplierUpdateForm, supplierUpdateFormSchema } from '@/app/_types/validation-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+export default function UpdateSupplierForm({
+  defaultValues,
+  supplierId,
+  authUserId,
+}: {
+  defaultValues: SupplierUpdateForm
+  supplierId: string
+  authUserId: string
+}) {
+  const form = useForm<SupplierUpdateForm>({
+    resolver: zodResolver(supplierUpdateFormSchema),
+    defaultValues: {
+      ...defaultValues,
+    },
+  })
+
+  async function onSubmit(data: SupplierUpdateForm) {
+    console.log('onSubmit', data, supplierId, authUserId)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={cn('grid gap-close-friend')}>
+        <div className="grid gap-sibling">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormFieldItem label="Business name">
+                <FormControl>
+                  <Input {...field} required />
+                </FormControl>
+              </FormFieldItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="websiteUrl"
+            render={({ field }) => (
+              <FormFieldItem label="Website">
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormFieldItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormFieldItem label="Description">
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+              </FormFieldItem>
+            )}
+          />
+        </div>
+
+        <div className="flex justify-end gap-close-friend">
+          <Button variant={'ghost'} type="button" onClick={() => form.reset(defaultValues)} disabled={!form.formState.isDirty || form.formState.isSubmitting}>
+            Cancel
+          </Button>
+          <SubmitButton pendingChildren="Saving..." type="submit" disabled={!form.formState.isDirty || form.formState.isSubmitting}>
+            Save Changes
+          </SubmitButton>
+        </div>
+      </form>
+    </Form>
+  )
+}
