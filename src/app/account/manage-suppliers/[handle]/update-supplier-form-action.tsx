c2a318ee036a4909c49226d520052073912e3b9c
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-03 18:17:44 +1300

    Include authentication and validation checks

--- src/app/account/manage-suppliers/[handle]/update-supplier-form-action.tsx
+++ src/app/account/manage-suppliers/[handle]/update-supplier-form-action.tsx
@@ -1,8 +1,19 @@
 'use server'
 
-import { SupplierUpdateForm } from '@/app/_types/validation-schema'
+import { OPERATION_ERROR } from '@/app/_types/errors'
+import { SupplierUpdateForm, supplierUpdateFormSchema } from '@/app/_types/validation-schema'
+import { supplierOperations } from '@/operations/supplier-operations'
+import { getAuthUserId } from '@/utils/auth'
 
 export async function updateSupplierFormAction(supplierId: string, data: SupplierUpdateForm, submittedBy: string): Promise<SupplierUpdateForm> {
+  const authUserId = await getAuthUserId()
+  if (!authUserId || submittedBy !== authUserId) throw OPERATION_ERROR.NOT_AUTHENTICATED()
+
+  const { success, error, data: validatedData } = supplierUpdateFormSchema.safeParse(data)
+  if (!success || error) throw OPERATION_ERROR.VALIDATION_ERROR()
+
+  supplierOperations.updateProfile(supplierId, validatedData, authUserId)
+
   console.log('updateSupplierFormAction', data)
   return data
 }
