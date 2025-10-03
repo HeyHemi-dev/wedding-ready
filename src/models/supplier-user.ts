f2cf23339bd7806f60dd1a90990f28728a8a2e32
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-03 18:20:03 +1300

    Retrieve supplier users by supplier ID

--- src/models/supplier-user.ts
+++ src/models/supplier-user.ts
@@ -2,9 +2,15 @@ import { db } from '@/db/connection'
 import { SupplierRole } from '@/db/constants'
 import * as s from '@/db/schema'
 import * as t from '@/models/types'
+import { eq } from 'drizzle-orm'
 
 export const supplierUsersModel = {
+  getForSupplierId,
   createForSupplierId,
+}
+
+async function getForSupplierId(supplierId: string): Promise<t.SupplierUserRaw[]> {
+  return await db.select().from(s.supplierUsers).where(eq(s.supplierUsers.supplierId, supplierId))
 }
 
 async function createForSupplierId({ supplierId, users }: { supplierId: string; users: { id: string; role: SupplierRole }[] }): Promise<t.SupplierUserRaw[]> {
