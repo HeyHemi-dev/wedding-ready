82eb0c77a7f0f32fe2db26000b125c044c966ae6
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-20 20:31:30 +1300

    Rename file

--- src/components/card/feature-card.tsx
+++ src/components/card/feature-card.tsx
@@ -8,7 +8,7 @@ interface FeatureCardProps {
 
 export function FeatureCard({ Icon, title, description }: FeatureCardProps) {
   return (
-    <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] gap-x-partner gap-y-spouse rounded-area border border-area p-6">
+    <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] gap-x-partner gap-y-spouse rounded-area border border-area p-6 transition-all duration-200 hover:border-primary hover:bg-primary/20">
       <Icon className="h-6 w-6 self-center text-muted-foreground" />
       <h3 className="ui-large col-start-2">{title}</h3>
       <p className="ui col-start-2 text-pretty text-muted-foreground">{description}</p>
