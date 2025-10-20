4593f56e4cf7187b9db2b9714e32532c527f8f46
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-20 20:08:18 +1300

    Rename and update styling to limit content width for improved layout

--- src/components/suppliers/faq-item.tsx
+++ src/components/suppliers/faq-item.tsx
@@ -4,13 +4,13 @@ import { ChevronDown } from 'lucide-react'
 
 import { cn } from '@/utils/shadcn-utils'
 
-interface FAQItemProps {
+interface FAQProps {
   question: string
   content: ReactNode
   className?: string
 }
 
-export function FAQItem({ question, content, className }: FAQItemProps) {
+export function FAQ({ question, content, className }: FAQProps) {
   return (
     <details name={'faq'} className={cn('group', className)}>
       <summary className="flex cursor-pointer list-none items-center justify-between gap-sibling">
@@ -18,7 +18,7 @@ export function FAQItem({ question, content, className }: FAQItemProps) {
         <ChevronDown className="h-6 w-6 text-muted-foreground transition-transform duration-200 group-open:rotate-180" />
       </summary>
       <div className="-translate-y-4 py-6 text-foreground/80 opacity-0 transition-all duration-200 group-open:translate-y-0 group-open:opacity-100">
-        <div className="prose ui flex flex-col gap-sibling text-pretty">{content}</div>
+        <div className="prose ui flex max-w-prose flex-col gap-sibling text-pretty">{content}</div>
       </div>
     </details>
   )
