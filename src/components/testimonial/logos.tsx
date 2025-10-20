d234fb20ef76c10b7362b444aea1c6362e328d4d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-20 19:46:28 +1300

    Refactor Logos component to simplify props and improve title display; move title rendering to a paragraph element for better accessibility

--- src/components/testimonial/logos.tsx
+++ src/components/testimonial/logos.tsx
@@ -7,32 +7,13 @@ interface Logo {
 
 interface LogosProps {
   title?: string
-
   logos?: Logo[]
 }
 
-export function Logos({
-  title = 'Trusted by New Zealand suppliers',
-
-  logos = [
-    {
-      name: 'Together Journal',
-      logo: '/assets/why-join-logos/together-transparent-black.png',
-    },
-    {
-      name: 'Hooray',
-      logo: '/assets/why-join-logos/hooray-transparent.png',
-    },
-    {
-      name: 'Hello May',
-      logo: '/assets/why-join-logos/hello-may-logo-transparent.png',
-    },
-  ],
-}: LogosProps) {
+export function Logos({ title, logos = [] }: LogosProps) {
   return (
     <div className="grid gap-friend">
-      <h2 className="ui text-center text-muted-foreground">{title}</h2>
-      <div className="order-first flex flex-wrap items-center justify-around gap-sibling">
+      <div className="flex flex-wrap items-center justify-around gap-sibling">
         {logos.map((logo) => (
           <React.Fragment key={logo.name}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -44,6 +25,7 @@ export function Logos({
           </React.Fragment>
         ))}
       </div>
+      <p className="ui text-center text-muted-foreground">{title}</p>
     </div>
   )
 }
