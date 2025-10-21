2168bcab78ec05a8b3575467d36622cb8e6de7c9
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-10-21 19:46:53 +1300

    Use userOperations for fetching user data

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,30 +1,31 @@
 import { redirect } from 'next/navigation'
 
-import { getCurrentUser } from '@/app/_actions/get-current-user'
-import { nullishToEmptyString } from '@/utils/empty-strings'
-
 import UpdateProfileForm from './update-profile-form'
+import { userOperations } from '@/operations/user-operations'
+import { getAuthUserId } from '@/utils/auth'
 
 export default async function AccountPage() {
-  const user = await getCurrentUser()
+  const authUserId = await getAuthUserId()
 
-  if (!user) {
+  if (!authUserId) {
     redirect('/sign-in')
   }
+
+  const user = await userOperations.getById(authUserId)
 
   return (
     <div className="grid gap-friend">
       <h1 className="heading-lg">Update your personal profile</h1>
       <UpdateProfileForm
-        defaultValues={nullishToEmptyString({
+        defaultValues={{
           id: user.id,
           displayName: user.displayName,
-          bio: user.bio,
-          avatarUrl: user.avatarUrl,
-          instagramUrl: user.instagramUrl,
-          tiktokUrl: user.tiktokUrl,
-          websiteUrl: user.websiteUrl,
-        })}
+          bio: user.bio ?? '',
+          avatarUrl: user.avatarUrl ?? '',
+          instagramUrl: user.instagramUrl ?? '',
+          tiktokUrl: user.tiktokUrl ?? '',
+          websiteUrl: user.websiteUrl ?? '',
+        }}
         className="max-w-lg"
       />
       <pre className="whitespace-pre-wrap rounded border bg-muted p-3 font-mono text-xs text-muted-foreground">{JSON.stringify(user, null, 2)}</pre>
