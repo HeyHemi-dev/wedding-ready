a1a97798f1d4243be9115f0ece6f50912c76deb8
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-11-10 10:29:39 +1300

    Rename user_details table to user_profiles

---
+++ src/db/migrations/0007_dark_darkstar.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "user_details" RENAME TO "user_profiles";--> statement-breakpoint
+ALTER TABLE "user_profiles" DROP CONSTRAINT "user_details_handle_unique";--> statement-breakpoint
+ALTER TABLE "user_profiles" DROP CONSTRAINT "user_details_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "user_profiles" ADD CONSTRAINT "user_profiles_id_users_id_fk" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_profiles" ADD CONSTRAINT "user_profiles_handle_unique" UNIQUE("handle");
