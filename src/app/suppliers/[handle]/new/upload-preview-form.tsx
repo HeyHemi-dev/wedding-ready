3f7fc611dfb453ff2c95188895ebea9f9587f966
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-11-19 11:01:31 +1300

    Refactor UploadPreviewForm navigation logic
    - Removed validation check for the location field in the first step.
    - Introduced a new handleBack function for better navigation between steps.
    - Updated button click handlers to improve code readability.

--- src/app/suppliers/[handle]/new/upload-preview-form.tsx
+++ src/app/suppliers/[handle]/new/upload-preview-form.tsx
@@ -45,15 +45,15 @@ export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; sta
   })
   const [formStep, setFormStep] = React.useState<FormStep>(formSteps[0])
 
-  const locationFieldState = form.getFieldState('location')
-  const locationValue = form.watch('location')
-  const isStep1Valid = Boolean(locationValue) && !locationFieldState.invalid
-
   async function handleNext() {
     const isValid = await form.trigger(['location'])
     if (isValid) {
       setFormStep(formSteps[1])
     }
+  }
+
+  async function handleBack() {
+    setFormStep(formSteps[0])
   }
 
   async function onSubmit(data: TileUploadPreviewForm) {
@@ -143,7 +143,7 @@ export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; sta
         )}
         <div data-test-id="form-footer" className="grid grid-cols-[1fr_auto_auto] gap-sibling">
           {formStep === formSteps[1] && (
-            <Button variant="default" type="button" onClick={() => setFormStep(formSteps[0])}>
+            <Button variant="default" type="button" onClick={handleBack}>
               Back
             </Button>
           )}
@@ -151,7 +151,7 @@ export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; sta
             Cancel
           </Button>
           {formStep === formSteps[0] ? (
-            <Button variant="default" type="button" onClick={handleNext} disabled={!isStep1Valid}>
+            <Button variant="default" type="button" onClick={handleNext}>
               Next
             </Button>
           ) : (
