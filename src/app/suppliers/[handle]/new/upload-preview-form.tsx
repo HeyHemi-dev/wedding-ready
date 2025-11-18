c0f3d1273c726896a830ebcf90d7b03b547075ce
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-11-19 10:26:12 +1300

    Enhance UploadPreviewForm with multi-step functionality
    - Introduced form steps for better user navigation: "Add Details" and "Credit Suppliers".
    - Added state management for form steps and navigation buttons.
    - Improved layout and organization of form fields for clarity.

--- src/app/suppliers/[handle]/new/upload-preview-form.tsx
+++ src/app/suppliers/[handle]/new/upload-preview-form.tsx
@@ -14,6 +14,11 @@ import { Textarea } from '@/components/ui/textarea'
 import { locationHelpers } from '@/utils/const-helpers'
 
 import { UploadItem, useUploadContext } from './upload-context'
+import { Button } from '@/components/ui/button'
+
+const formSteps = ['Add Details', 'Credit Suppliers'] as const
+
+type FormStep = (typeof formSteps)[number]
 
 export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; startUpload: (files: File[], data: TileUploadPreviewForm) => void }) {
   const { supplier, authUserId } = useUploadContext()
@@ -37,6 +42,7 @@ export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; sta
     },
     mode: 'onBlur',
   })
+  const [formStep, setFormStep] = React.useState<FormStep>(formSteps[0])
 
   async function onSubmit(data: TileUploadPreviewForm) {
     // startUpload catches and handles errors
@@ -45,80 +51,103 @@ export function UploadPreviewForm({ file, startUpload }: { file: UploadItem; sta
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-friend">
-        <div className="grid gap-sibling">
-          <h3 className="ui-s1">Tile Details</h3>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-rows-[auto_1fr] gap-friend">
+        {formStep === formSteps[0] && (
+          <div data-test-id={`form-step-1`} className="grid grid-cols-2 gap-sibling">
+            <div data-test-id="form-header" className="flex gap-spouse">
+              <p className="ui text-muted-foreground">1/{formSteps.length}</p>
+              <h3 className="ui-s1 col-span-full">{formSteps[0]}</h3>
+            </div>
 
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormFieldItem label="Title">
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-              </FormFieldItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormFieldItem label="Title">
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormFieldItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormFieldItem label="Description">
-                <FormControl>
-                  <Textarea {...field} rows={3} />
-                </FormControl>
-              </FormFieldItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormFieldItem label="Location">
+                  <FormControl>
+                    <Select onValueChange={field.onChange} value={field.value || ''}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a location" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {locationHelpers.toPretty().map((location) => (
+                          <SelectItem key={location.value} value={location.value}>
+                            {location.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormFieldItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="location"
-            render={({ field }) => (
-              <FormFieldItem label="Location">
-                <FormControl>
-                  <Select onValueChange={field.onChange} value={field.value || ''}>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a location" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {locationHelpers.toPretty().map((location) => (
-                        <SelectItem key={location.value} value={location.value}>
-                          {location.label}
-                        </SelectItem>
+            <div className="col-span-full">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormFieldItem label="Description">
+                    <FormControl>
+                      <Textarea {...field} rows={3} />
+                    </FormControl>
+                  </FormFieldItem>
+                )}
+              />
+            </div>
+          </div>
+        )}
+        {formStep === formSteps[1] && (
+          <div className="grid auto-rows-max gap-sibling">
+            <h3 className="ui-s1">Suppliers</h3>
+
+            <FormField
+              control={form.control}
+              name="credits"
+              render={({ field }) => (
+                <FormFieldItem label="Credit suppliers">
+                  <FormControl>
+                    <div className="text-sm text-muted-foreground">
+                      {field.value.map((credit) => (
+                        <div key={credit.supplierId}>
+                          {credit.supplierId} - {credit.service}
+                        </div>
                       ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-              </FormFieldItem>
-            )}
-          />
-        </div>
-        <div className="grid auto-rows-max gap-sibling">
-          <h3 className="ui-s1">Suppliers</h3>
-
-          <FormField
-            control={form.control}
-            name="credits"
-            render={({ field }) => (
-              <FormFieldItem label="Credit suppliers">
-                <FormControl>
-                  <div className="text-sm text-muted-foreground">
-                    {field.value.map((credit) => (
-                      <div key={credit.supplierId}>
-                        {credit.supplierId} - {credit.service}
-                      </div>
-                    ))}
-                  </div>
-                </FormControl>
-              </FormFieldItem>
-            )}
-          />
-        </div>
-        <div className="col-span-full">
-          <SubmitButton pendingChildren={'Please wait'}>Upload</SubmitButton>
+                    </div>
+                  </FormControl>
+                </FormFieldItem>
+              )}
+            />
+          </div>
+        )}
+        <div data-test-id="form-footer" className="grid grid-cols-[1fr_auto_auto] gap-sibling">
+          {formStep === formSteps[1] && (
+            <Button variant="default" type="button" onClick={() => setFormStep(formSteps[0])}>
+              Back
+            </Button>
+          )}
+          <Button variant="ghost" type="button">
+            Cancel
+          </Button>
+          {formStep === formSteps[0] ? (
+            <Button variant="default" type="button" onClick={() => setFormStep(formSteps[1])}>
+              Next
+            </Button>
+          ) : (
+            <SubmitButton pendingChildren={'Please wait'}>Upload</SubmitButton>
+          )}
         </div>
       </form>
     </Form>
