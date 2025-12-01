33cbf3c1b607ed6209e725d94ee459dcc94afbaf
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-01 13:30:13 +1300

    Add warning for unsaved changes on page unload in UploadLayout component

--- src/app/suppliers/[handle]/new/upload-layout.tsx
+++ src/app/suppliers/[handle]/new/upload-layout.tsx
@@ -1,11 +1,32 @@
 'use client'
 
+import * as React from 'react'
+
 import { useUploadContext } from './upload-context'
 import { UploadDropzone } from './upload-dropzone'
 import { UploadPreviewList } from './upload-preview'
 
 export function UploadLayout() {
   const { files } = useUploadContext()
+
+  // Warn user before leaving if there are files (form data or uploads in progress)
+  React.useEffect(() => {
+    if (files.length === 0) {
+      return
+    }
+
+    function handleBeforeUnload(event: BeforeUnloadEvent) {
+      event.preventDefault()
+      // Modern browsers ignore custom messages and show their own
+      event.returnValue = ''
+    }
+
+    window.addEventListener('beforeunload', handleBeforeUnload)
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [files.length])
 
   return <>{files.length > 0 ? <UploadPreviewList /> : <UploadDropzone />}</>
 }
