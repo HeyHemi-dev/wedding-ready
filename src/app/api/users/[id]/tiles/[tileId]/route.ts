ab76b018daae1243dcc54853eaa9925a64fd03bf
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-08 16:06:31 +1300

    Refactor tile route handlers to improve type safety and error handling, updating GET and POST methods to return RouteResponse types and enhancing error management with tryCatch.

--- src/app/api/users/[id]/tiles/[tileId]/route.ts
+++ src/app/api/users/[id]/tiles/[tileId]/route.ts
@@ -1,7 +1,7 @@
 import { HTTP_ERROR } from '@/app/_types/errors'
+import { RouteResponse } from '@/app/_types/generics'
 import { TileSaveState, tileSaveStateSchema } from '@/app/_types/validation-schema'
 import { savedTilesModel } from '@/models/saved-tiles'
-import * as t from '@/models/types'
 import { tileOperations } from '@/operations/tile-operations'
 import { getAuthUserId } from '@/utils/auth'
 import { tryCatch } from '@/utils/try-catch'
@@ -10,23 +10,25 @@ import { NextResponse } from 'next/server'
 export type SaveTileGetResponseBody = TileSaveState
 
 // Get the saved state of a tile
-export async function GET(req: Request, { params }: { params: Promise<{ id: string; tileId: string }> }): Promise<NextResponse> {
+export async function GET(req: Request, { params }: { params: Promise<{ id: string; tileId: string }> }): Promise<RouteResponse<SaveTileGetResponseBody>> {
   const { id, tileId } = await params
+
   const authUserId = await getAuthUserId()
   if (!authUserId || authUserId !== id) {
     return HTTP_ERROR.UNAUTHORIZED()
   }
 
-  const savedTile = await savedTilesModel.getRaw(tileId, id)
+  const { data: savedTile, error } = await tryCatch(savedTilesModel.getRaw(tileId, id))
+  if (error) return HTTP_ERROR.INTERNAL_SERVER_ERROR()
 
-  return NextResponse.json(savedTile)
+  return NextResponse.json({ isSaved: savedTile?.isSaved ?? false })
 }
 
 export type SaveTilePostRequestBody = TileSaveState
 export type SaveTilePostResponseBody = TileSaveState
 
 // Save/unsave a tile
-export async function POST(req: Request, { params }: { params: Promise<{ id: string; tileId: string }> }): Promise<NextResponse> {
+export async function POST(req: Request, { params }: { params: Promise<{ id: string; tileId: string }> }): Promise<RouteResponse<SaveTilePostResponseBody>> {
   const { id, tileId } = await params
 
   const { data: body, error: parseError } = await tryCatch(req.json())
@@ -40,8 +42,8 @@ export async function POST(req: Request, { params }: { params: Promise<{ id: str
     return HTTP_ERROR.UNAUTHORIZED()
   }
 
-  const { data: savedTile, error } = await tryCatch(tileOperations.upsertSavedState(tileId, authUserId, body.isSaved ?? true))
+  const { data: saveState, error } = await tryCatch(tileOperations.upsertSavedState(tileId, authUserId, body.isSaved ?? true))
   if (error) return HTTP_ERROR.INTERNAL_SERVER_ERROR()
 
-  return NextResponse.json(savedTile)
+  return NextResponse.json(saveState)
 }
