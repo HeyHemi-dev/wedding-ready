b141e272448d85fdc40c948d1425defc7355bd07
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-09 09:39:25 +1300

    Implement authorization and error handling in GET route for score updates

--- src/app/api/cron/update-scores/route.ts
+++ src/app/api/cron/update-scores/route.ts
@@ -1,3 +1,17 @@
-export function GET(request: Request) {
-  return new Response('Scores updated')
+import { tileOperations } from '@/operations/tile-operations'
+import { RouteResponse } from '@/app/_types/generics'
+
+import { NextResponse, type NextRequest } from 'next/server'
+import { HTTP_ERROR } from '@/app/_types/errors'
+import { tryCatch } from '@/utils/try-catch'
+
+export async function GET(request: NextRequest): Promise<RouteResponse<{ success: boolean }>> {
+  const authHeader = request.headers.get('authorization')
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return HTTP_ERROR.UNAUTHORIZED()
+  }
+  const { error } = await tryCatch(tileOperations.updateScores())
+  if (error) return HTTP_ERROR.INTERNAL_SERVER_ERROR()
+
+  return NextResponse.json({ success: true })
 }
