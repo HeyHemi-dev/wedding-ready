3c71da634dd530d01318c1567c7b7fb757392b71
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-20 17:58:43 +1300

    Refactor createClient and createAdminClient functions to use new supabase keys

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -11,21 +11,18 @@ import { cookies } from 'next/headers'
  * const supabase = await createClient()
  * const { data, error } = await supabase.auth.getUser()
  */
-export const createClient = async () => {
+export async function createClient() {
   const cookieStore = await cookies()
-
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!, {
     cookies: {
       getAll() {
         return cookieStore.getAll()
       },
       setAll(cookiesToSet) {
         try {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            cookieStore.set(name, value, options)
-          })
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
         } catch (error) {
-          // The `set` method was called from a Server Component.
+          // The `setAll` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
           // user sessions.
           console.error(error)
@@ -48,7 +45,7 @@ export const createClient = async () => {
  * and only for admin operations like user management
  */
 export const createAdminClient = () => {
-  return createSupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
+  return createSupabaseClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET_KEY!, {
     auth: {
       autoRefreshToken: false,
       persistSession: false,
