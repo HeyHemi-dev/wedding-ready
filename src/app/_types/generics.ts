67eff756f30a7fd60479b5b1c2d090313bfd57d5
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-26 19:27:32 +1300

    Add SearchParams type to generics for improved type handling in API requests

--- src/app/_types/generics.ts
+++ src/app/_types/generics.ts
@@ -10,6 +10,8 @@ export type HttpErrorResponseBody = {
   message?: string
 }
 
+export type SearchParams = Record<string, string | string[] | undefined>
+
 /**
  * Helper type for API route responses that can return either a success body or an HTTP error.
  * Use this type for the return type of route handlers.
