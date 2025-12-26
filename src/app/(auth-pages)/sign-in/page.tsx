54b7b2aee306d77d04cd69748a5526190f485b24
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-26 20:05:54 +1300

    Enhance sign-in page to display form messages by integrating message parsing and validation, improving user feedback during authentication.

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -2,16 +2,24 @@ import { redirect } from 'next/navigation'
 
 import { getAuthUserId } from '@/utils/auth'
 import { AuthCard } from '@/components/auth/auth-card'
+import { FormMessage, messageSchema } from '@/components/form/form-message'
 
 import { LoginWithEmailPasswordFormButton } from './login-with-email-password-form'
 import LoginWithGoogleForm from './login-with-google-form'
+import { PARAMS } from '@/utils/constants'
+import { SearchParams } from '@/app/_types/generics'
+import { parseSearchParams } from '@/utils/api-helpers'
+import { tryCatch } from '@/utils/try-catch'
 
-export default async function Login() {
+export default async function Login(props: { searchParams: Promise<SearchParams> }) {
   // If user is already logged in, they don't need to be here.
   const authUserId = await getAuthUserId()
   if (authUserId) {
     redirect('/feed')
   }
+
+  const searchParams = await props.searchParams
+  const { data: message } = await tryCatch(parseSearchParams(searchParams, messageSchema))
 
   {
     return (
@@ -19,6 +27,7 @@ export default async function Login() {
         <LoginWithGoogleForm />
         <hr />
         <LoginWithEmailPasswordFormButton />
+        {message && <FormMessage message={message} />}
       </AuthCard>
     )
   }
