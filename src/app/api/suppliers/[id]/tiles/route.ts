571dea2a5a82e9c0280373bd3ab0d8c498ecdccc
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-26 20:06:05 +1300

    Refactor GET request handler in supplier tiles API to utilize new search parameter functions, improving query parameter parsing and validation while enhancing authentication checks.

--- src/app/api/suppliers/[id]/tiles/route.ts
+++ src/app/api/suppliers/[id]/tiles/route.ts
@@ -1,25 +1,28 @@
 import { NextResponse, NextRequest } from 'next/server'
 
 import { tileOperations } from '@/operations/tile-operations'
-import { parseQueryParams } from '@/utils/api-helpers'
+import { parseQueryParams, parseSearchParams, urlSearchParamsToObject } from '@/utils/api-helpers'
 import { getAuthUserId } from '@/utils/auth'
 import { tryCatch } from '@/utils/try-catch'
 
 import { supplierTilesGetRequestSchema, type SupplierTilesGetResponse } from './types'
+import { SearchParams } from '@/app/_types/generics'
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }): Promise<NextResponse> {
   const supplierId = (await params).id
-  const parsedQueryParams = parseQueryParams(req.nextUrl, supplierTilesGetRequestSchema)
+
+  const searchParams = urlSearchParamsToObject(req.nextUrl.searchParams)
+  const { data: parsedParams } = await tryCatch(parseSearchParams(searchParams, supplierTilesGetRequestSchema))
 
   // Only check authentication if an authUserId is provided
-  if (parsedQueryParams.authUserId) {
+  if (parsedParams) {
     const authUserId = await getAuthUserId()
-    if (!authUserId || authUserId !== parsedQueryParams.authUserId) {
+    if (!authUserId || authUserId !== parsedParams.authUserId) {
       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
     }
   }
 
-  const { data, error } = await tryCatch(tileOperations.getListForSupplier(supplierId, parsedQueryParams.authUserId))
+  const { data, error } = await tryCatch(tileOperations.getListForSupplier(supplierId, parsedParams?.authUserId))
 
   if (error) {
     return NextResponse.json({ message: 'Error fetching tiles', error: error.message }, { status: 500 })
