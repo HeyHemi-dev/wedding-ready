92634d3f0c461c3068d0f34e67720b4c5de5ee0f
Hemi Phillips <hello.hemi.phillips@gmail.com>
2025-12-26 20:05:44 +1300

    Refactor FormMessage component to utilize Zod for message validation and improve type handling, enhancing clarity and maintainability in message rendering.

--- src/components/form/form-message.tsx
+++ src/components/form/form-message.tsx
@@ -1,15 +1,33 @@
 import { PARAMS } from '@/utils/constants'
+import { cn } from '@/utils/shadcn-utils'
+import z from 'zod'
 
-export type Message = { [PARAMS.SUCCESS]: string } | { [PARAMS.ERROR]: string } | { [PARAMS.MESSAGE]: string }
+// export type Message = { [PARAMS.SUCCESS]: string } | { [PARAMS.ERROR]: string } | { [PARAMS.MESSAGE]: string }
+
+const MESSAGE_TYPES = ['success', 'error', 'info'] as const
+export const messageSchema = z.object({
+  [PARAMS.MESSAGE]: z
+    .string()
+    .min(1)
+    .max(255)
+    .regex(/^[a-zA-Z0-9\s]+$/),
+  [PARAMS.MESSAGE_TYPE]: z.enum(MESSAGE_TYPES),
+})
+
+export type Message = z.infer<typeof messageSchema>
 
 export function FormMessage({ message }: { message: Message }) {
   return (
     <div className="ui-small flex w-full max-w-md flex-col gap-sibling">
-      {'success' in message && <div className="rounded border border-border bg-secondary px-md py-sm text-secondary-foreground">{message.success}</div>}
-
-      {'error' in message && <div className="rounded border border-destructive/50 bg-destructive/10 px-md py-sm text-destructive">{message.error}</div>}
-
-      {'message' in message && <div className="rounded border border-amber-600/50 bg-amber-600/10 px-md py-sm text-foreground">{message.message}</div>}
+      <div
+        className={cn(
+          'rounded border border-border bg-background px-md py-sm text-foreground',
+          message.mtype === MESSAGE_TYPES[0] && 'bg-secondary text-secondary-foreground',
+          message.mtype === MESSAGE_TYPES[1] && 'bg-destructive/10 text-destructive',
+          message.mtype === MESSAGE_TYPES[2] && 'bg-amber-600/10 text-foreground'
+        )}>
+        {message.m}
+      </div>
     </div>
   )
 }
