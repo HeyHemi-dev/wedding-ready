5681d039ce8c9bd0d6ef950deb6fe081baf62b3d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-01-08 19:36:50 +1300

    Add table component to KitchensinkPage: integrate a dynamic table with customizable rows and columns, enhancing the page's functionality and layout.

--- src/app/kitchensink/page.tsx
+++ src/app/kitchensink/page.tsx
@@ -1,3 +1,4 @@
+import { Table, TableRow, TableCell } from '@/components/pricing/table'
 import { AddCreditButton } from '@/components/tiles/add-credit-button'
 import { Area } from '@/components/ui/area'
 import { Section } from '@/components/ui/section'
@@ -14,6 +15,34 @@ export default function KitchensinkPage() {
         <Area className="grid gap-friend">
           <AddCreditButton tileId="123" />
         </Area>
+      </Section>
+      <Section>
+        {(() => {
+          const numColumns = 4
+          const numRows = 3
+
+          return (
+            <Table
+              isFirstColFrozen
+              className="ui-small"
+              style={{ gridTemplateRows: `repeat(${numRows}, max-content)`, gridTemplateColumns: `repeat(${numColumns}, minmax(20rem, 1fr))` }}>
+              <TableRow hasAccentOnHover={false} className="ui-small-s1">
+                {Array.from({ length: numColumns }).map((_, index) => (
+                  <TableCell key={index}>Header {index + 1}</TableCell>
+                ))}
+              </TableRow>
+              {Array.from({ length: numRows }).map((_, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {Array.from({ length: numColumns }).map((_, colIndex) => (
+                    <TableCell key={colIndex}>
+                      {rowIndex + 1}-{colIndex + 1}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </Table>
+          )
+        })()}
       </Section>
     </>
   )
