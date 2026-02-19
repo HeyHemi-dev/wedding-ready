ea76f0816ef99f1a43bf4ec9ffe12c3a8ecf21dd
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-02-19 16:48:34 +1300

    Refactor ActionBar component to use updated spacing variable; rename CSS variable for clarity

--- src/components/action-bar/action-bar.tsx
+++ src/components/action-bar/action-bar.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react'
 
 import { cn } from '@/utils/shadcn-utils'
 
-
 export function ActionBar({ children, className }: { children: ReactNode; className?: string }) {
-  return <div className={cn('h-action-bar grid items-center rounded-full bg-secondary p-xxs', className)}>{children}</div>
+  return <div className={cn('h-action-bar bg-secondary p-2xs grid items-center rounded-full', className)}>{children}</div>
 }
